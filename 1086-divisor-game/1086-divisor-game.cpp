6476d4f538cc10d141bb36a15bf5aed462287ec3
AntimaTiwari07 <153281706+AntimaTiwari07@users.noreply.github.com>
2024-09-22 23:28:19 +0530

    Time: 3 ms (25.32%), Space: 7.4 MB (62.69%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        bool win;
+            if((n%2==0)){
+                win = true;   
+            }
+            else{
+                win = false;
+    
+            }
+        
+        return win;
+    }
+};
