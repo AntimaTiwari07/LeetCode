fe9d8dfefb8a482f945ae763835640c55d8c7e91
AntimaTiwari07 <153281706+AntimaTiwari07@users.noreply.github.com>
2024-09-22 22:33:26 +0530

    Time: 17 ms (58.3%), Space: 26.8 MB (98.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /*
+        //my first approach (brute force)
         int ans = 0;
+        //to count no. of element
         int count ;
+        //iterate through each no;
         for(int i = 0;i<nums.size();i++){
             count = 0;
+            //check if equal or not
             for(int j = i+1;j<nums.size();j++){
                 if(nums[i]==nums[j]){
                     count++;
                 }
             }
+            //check count is greater then n/2
             if((count+1)>((nums.size()-1)/2)){
                     ans = nums[i];
                     break;
             } 
+        }
+        return ans;
+        */
+        int freq = 0,ans=0;
+        for(int i = 0;i<nums.size();i++){
+            if(freq==0){
+                ans = nums[i];
+            }
+            if(ans==nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
         }
         return ans;
     }
