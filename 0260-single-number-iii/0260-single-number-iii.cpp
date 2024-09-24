1f374d7a453725d11f5d06e396ee90f3e5ef0fea
AntimaTiwari07 <153281706+AntimaTiwari07@users.noreply.github.com>
2024-09-25 01:14:19 +0530

    Time: 7 ms (61.88%), Space: 12.9 MB (36.92%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,21 +3,17 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         vector<int>v;
         sort(nums.begin(),nums.end());
-        int freq = 1;
-        int i = 0;
-        while(i<nums.size()){
-            int j = i+1;
-            freq = 1;
-            while(j<nums.size()){
-               if(nums[i]==nums[j]){
-                freq++;
+        int i = 1;
+        while(i<nums.size()){     
+           if(nums[i-1]!=nums[i]){
+                 v.push_back(nums[i-1]);
+                 i++;
+                 continue;
                }
-               j++;
+            i = i+2;
             }
-            if(freq==1){
-                v.push_back(nums[i]);
-            }
-            i = i +freq;
+            if(v.size()==1){
+                v.push_back(nums[nums.size()-1]);
             }
         return v;
     }
