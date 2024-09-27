2bfc234a77bee208e39336c05240a954c1b05611
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-09-28 00:04:07 +0530

    Time: 5 ms (34.94%), Space: 8.4 MB (19.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,9 +3,10 @@ public:
     int mySqrt(int x) {
         long num = x;
         long start = 1;
-        long end = x;
+        long end = x/2;
         long mid;
         long ans =0;
+        if(x==1) return 1;
         while(start<=end){
           mid = start +(end-start)/2;
           if((mid*mid)<=num){
