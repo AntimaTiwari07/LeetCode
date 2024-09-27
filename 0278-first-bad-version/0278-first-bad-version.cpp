7312eae99dfd1c5d88c36b9e749330e9d6a818f8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-09-27 23:43:22 +0530

    Time: 2 ms (58.42%), Space: 7.4 MB (98.01%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -6,7 +6,7 @@ public:
     int firstBadVersion(int n) {
        int start = 1;
        int end = n;
-       int mid = start +(end-start)/2;
+       int mid ;
        int ans =-1;
        while(start<=end){
         mid = start +(end-start)/2;
