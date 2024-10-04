6bc1199dd4c997122d3f809ad00c1fc42aa848df
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-04 23:38:26 +0530

    Time: 64 ms (58.2%), Space: 56.3 MB (92.6%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-          vector<long long >product;
-        bool correct = true ;
-         long long sum = 0;
-        sort(skill.begin(),skill.end());
-          int i = 0;
-          int j = skill.size()-1;
-          while((i<=(skill.size()/2)-1) &&(j>=skill.size()/2)){
-             product.push_back(skill[i]);
-             product.push_back(skill[j]);
-            i++;
-            j--;
-          }
-          int check_sum = product[0]+product[1];
-          for(int i = 2;i<product.size()-1;i+=2){
-            int temp = product[i]+product[i+1];
-                        if(temp!=check_sum){
-                   correct =false;
-                   break;
-            }
-           
-          }
          
-          if(correct == true){
-            for(int i = 0;i<product.size()-1;i+=2){
-                sum = sum + (product[i]*product[i+1]);
-            }
+        sort(skill.begin(),skill.end());
+          int i = 1;
+          int j = skill.size()-2;
+          long long chemistry = skill[0]*skill[skill.size()-1] ;
+          long long sum = skill[0]+skill[skill.size()-1];
+        bool check = true ;
+          while((i<j)&&(check)){
+             if(skill[i]+skill[j]==sum){
+                chemistry += skill[i]*skill[j];
+           i++;
+            j--;
+             }
+             else{
+                check = false;
+             } 
           }
-          else{
-            return -1;
-          }
-return sum;
+          if(check==false){return -1;}
+          else {return chemistry;}
     }
 };
