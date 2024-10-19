dba46d17cfa236746faafd649e22e8212ae1d2ef
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-20 01:35:39 +0530

    Time: 387 ms (5.13%), Space: 37 MB (52.99%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -6,12 +6,9 @@ public:
        while(i<nums.size()-1){
         if(nums[i]!=nums[i+1]){
             nums.erase(nums.begin()+i);
-            i --;
+            i--;
         }
             i++;
-        }
-        for(int i = 0;i<nums.size();i++){
-            cout<<nums[i]<<" ";
         }
         nums.pop_back();
         return nums;
