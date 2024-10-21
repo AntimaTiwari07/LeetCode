0deb9dcb3d52f907cba042fd85a8afd37ddb37e3
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-21 22:17:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (67.73%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {      
+void print_per(vector<int>& nums, vector<vector<int>>&permuatation,int idx){
+    if(idx==nums.size()){
+        permuatation.push_back(nums);
+       return;
+    }
+    for(int j = idx;j<nums.size();j++){
+        swap(nums[idx],nums[j]);
+        print_per(nums,permuatation,idx+1);
+        //backtracking
+        swap(nums[idx],nums[j]); 
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {    
+        /*  
          vector<vector<int>>permuatation;
         sort(nums.begin(),nums.end());
         permuatation.push_back(nums);       
@@ -8,5 +21,9 @@ public:
              permuatation.push_back(nums);
         }
            return permuatation;
+           */
+           vector<vector<int>>permuatation;
+            print_per(nums,permuatation,0);
+          return permuatation;
     }
 };
