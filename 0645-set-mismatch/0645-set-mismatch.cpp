dc09201ccb56b58bca0bc7ebc95b6e889132b652
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-23 22:06:04 +0530

    Time: 0 ms (100%), Space: 25 MB (51.37%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,21 +3,21 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int>missing;
+        int duplicate = 0;
         for(int i = 0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
+                duplicate = nums[i];
                   missing.push_back(nums[i]);
                   break;
             }
         }
-        vector<int>count(nums.size()+1,0);
-        for(int i  = 0;i<nums.size();i++){
-           count[nums[i]]++;
-        }
-        for(int i = 1;i<=nums.size();i++){
-            if(count[i]==0){
-                missing.push_back(i);
-            }
-        }
+       int sum = 0;
+       for(int i = 0;i<nums.size();i++){
+            sum+=nums[i];
+       }
+       int total_sum  = nums.size()*(nums.size()+1)/2;
+       missing.push_back((total_sum - sum )+duplicate);
+
         return missing;
 
     }
