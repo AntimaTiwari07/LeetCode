9f39eca645fc7e474d5f3d372393a585d17f127a
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-24 21:48:50 +0530

    Time: 121 ms (6.96%), Space: 74.2 MB (50%) - LeetHub

---
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        vector<int>max_num;
+        int ans = -1;
+        int maxn = 0;
+        for(int i = 0;i<nums.size();i++){
+            maxn = 0;
+            int number = nums[i];
+           while(number!=0){
+             maxn = max(maxn,number%10);
+             number = number/10;
+           }
+           max_num.push_back(maxn);
+        }
+        for(int i  = 0;i<max_num.size();i++){
+            cout<<max_num[i]<<" ";
+        }
+        for(int i = 0;i<max_num.size();i++){
+            for(int j = i+1;j<max_num.size();j++){
+                if(max_num[i]==max_num[j]){
+                   ans = max(ans,(nums[i]+nums[j]));
+                }
+            }
+        }
+        cout<<ans;
+        return ans;
+    }
+};
