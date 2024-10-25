3d2967598a5b5c5d94352c1fe4a9a01702accdd3
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-10-26 02:29:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (68.07%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int start = 1; // Starting from the first row
-        int end = n;   // The maximum possible rows can't be more than n
+        int s = 1;
+        int end = n;
         int mid;
-
-        while (start <= end) {
-            mid = start + (end - start) / 2; // Midpoint for binary search
-            long long x = (long long)mid * (mid + 1) / 2; // Total coins needed for `mid` rows
-            
-            if (x == n) {
-                return mid; // Exact match, we can use all `n` coins to form `mid` rows
-            } else if (x < n) {
-                start = mid + 1; // We can potentially add more rows
-            } else {
-                end = mid - 1; // We need to reduce the number of rows
+        long long x;
+        while(s<=end){
+            mid = s + (end-s)/2;
+             x = (long long )mid*(mid+1)/2;
+            if(x==n){
+                return mid;
+            }
+            else if(x<n){
+                s = mid+1;
+            }
+            else{
+                end = mid-1;
             }
         }
-
-        return end; // The maximum number of complete rows we can form
+        return end;
     }
 };
