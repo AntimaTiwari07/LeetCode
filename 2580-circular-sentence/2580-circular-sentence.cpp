f4faa64e873cdce204a6f96d67670815dbd76a4d
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-11-02 08:57:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        int cntspace = -1;
-        cout<<sentence.size();
-        for(int i = 0;i<sentence.size();i++){
-            if(sentence[i]==' '){
-                cout<<i<<endl;
-                cntspace=i;
-                }
-                if(cntspace!=-1){
-            if(sentence[cntspace-1]!=sentence[cntspace+1]){
-                cout<<sentence[cntspace-1]<<endl;
-                return false;
-            } 
-        }
-        }
-        if(sentence[0]!=sentence[sentence.size()-1]){
-            cout<<sentence[sentence.size()-1]<<" "<<sentence[0]<<endl;
+         if(sentence[0]!=sentence[sentence.size()-1]){
                 return false;
             }
+        for(int i = 0;i<sentence.size();i++){
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){
+                return false;
+            }
+        }
+        }
+       
         return true;
     }
 };
