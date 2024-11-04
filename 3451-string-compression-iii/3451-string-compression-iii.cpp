66f1e507fb56a6683b7a6cc4123fffea9777b7f8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-11-04 23:14:47 +0530

    Time: 48 ms (35.9%), Space: 30.9 MB (6.1%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     string compressedString(string word) {
-        if (word.empty()) return "";
-
-        string comp;
-        comp.reserve(word.size());  // Reserve estimated size
-
-        int count = 1;
-        for (int i = 0; i < word.size() - 1; i++) {
-            if (word[i] != word[i + 1] || count == 9) {
-                comp += to_string(count) + word[i];
-                count = 1;
-            } else {
-                count++;
-            }
-        }
-
-        comp += to_string(count) + word.back();  // Add the last character
-
-        return comp;
+    if(word.empty()) return "";
+    string comp ;
+    comp.reserve(word.size());
+      int count = 1;
+      for(int i = 0;i<word.size()-1;i++){
+        if(word[i]!=word[i+1] || count==9){
+            comp +=to_string(count)+word[i];
+            count = 1;
+      }
+      else{
+        count++;
+      }
+      }
+        comp = comp+to_string(count)+word.back();
+      return comp;
     }
 };
