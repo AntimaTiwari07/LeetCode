233ce648e01ceb3ac2916e72d6bf967b4301628c
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-11-08 22:06:06 +0530

    Time: 18 ms (12.38%), Space: 107.1 MB (5.15%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -4,14 +4,11 @@ public:
         vector<int>v;
         int n = nums.size();
         vector<int>prefix;
+             int k = pow(2,maximumBit)-1;
        prefix.push_back(nums[0]);
         for(int i = 1;i<n;i++){
             prefix.push_back(nums[i]^prefix[i-1]);
         }
-        for(int i = 0;i<prefix.size();i++){
-            cout<<prefix[i]<<" ";
-        }
-        int k = pow(2,maximumBit)-1;
         for(int i = 0;i<n;i++){
             v.push_back(prefix[i]^k);
         }
