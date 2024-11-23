35df2e686e53d357e51cdd3301cb52299fc7632a
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-11-24 00:28:24 +0530

    Time: 0 ms (100%), Space: 70.5 MB (77.39%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.cpp
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -4,8 +4,7 @@ public:
         long long count = 0;
         for(int i = s.size()-1;i>=0;i--){
            count = count +shifts[i];
-                int temp = ((s[i]-'a')+count)%26;
-                s[i] = temp+'a';
+                s[i] = ((s[i]-'a')+count)%26+'a';
             
         }
         return s; 
