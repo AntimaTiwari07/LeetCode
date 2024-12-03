078bb571de5922fc826d95217b222e1f1ccc42ba
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-03 23:28:18 +0530

    Time: 9 ms (63.48%), Space: 90 MB (49.52%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(original.size()!=m*n){return {};}
        vector<vector<int>>matrix(m,vector<int>(n));
-       if(original.size()!=m*n){return {};}
        for(int i = 0;i<m;i++){
         for(int j = 0;j<n;j++){
             matrix[i][j] = original[n*i+j];
