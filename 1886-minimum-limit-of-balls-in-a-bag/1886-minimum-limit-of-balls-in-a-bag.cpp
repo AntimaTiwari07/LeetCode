a802552a4a4dbacccb731d2fffdf934b330a57a0
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-08 15:16:49 +0530

    Time: 51 ms (24.12%), Space: 59.7 MB (22.57%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
+   bool minimumPanelty(int mid,vector<int>& nums,int maxOperations){
+    int totalop = 0;
+    for(int i = 0;i<nums.size();i++){      
+            int count = nums[i]/mid;
+            if(nums[i]%mid==0){
+                count-=1;
+            }  
+            totalop+=count;   
+    }
+     if(totalop>maxOperations){
+        return false;
+     }
+    return true;
+   }
     int minimumSize(vector<int>& nums, int maxOperations) {
-        int left = 1, right = *max_element(nums.begin(), nums.end());    
-        int ans = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2, ops = 0;
-
-            for (int n : nums) {
-                ops += (n - 1) / mid;
-                if (ops > maxOperations) break;
-            }
-
-            if (ops <= maxOperations) {
+        sort(nums.begin(),nums.end());
+        int st = 1;
+        int end = nums[nums.size()-1];
+        int mid ;
+        int ans = 0;
+        while(st<=end){
+            mid = st+(end-st)/2;
+            if(minimumPanelty(mid,nums,maxOperations)){
                 ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
             }
         }
-
         return ans;
     }
 };
