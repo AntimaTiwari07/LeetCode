d867daec531b1f38c1114fba4e4b1b3fbc377319
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-08 15:18:38 +0530

    Time: 47 ms (28.95%), Space: 59.8 MB (15.3%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
    bool minimumPanelty(int mid,vector<int>& nums,int maxOperations){
     int totalop = 0;
-    for(int i = 0;i<nums.size();i++){      
-            int count = nums[i]/mid;
+    for(int i = 0;i<nums.size();i++){  
+        totalop+=nums[i]/mid;      
             if(nums[i]%mid==0){
-                count-=1;
-            }  
-            totalop+=count;   
+                totalop-=1;
+            }     
     }
-     if(totalop>maxOperations){
-        return false;
-     }
-    return true;
+    return totalop>maxOperations?false : true;
    }
     int minimumSize(vector<int>& nums, int maxOperations) {
         sort(nums.begin(),nums.end());
