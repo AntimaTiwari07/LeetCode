ab7a47ac9073ce3804f3a1facd2c285b56966238
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-13 00:27:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (49.71%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if(n==1 || n==7 || n==10 ){return true;}
        int ans = n;
        unordered_set<int>v;
         while(ans>1){
             int temp = ans;
-            int val = 0;            
-            int count = 0;            
+            int val = 0;                     
          while(temp!=0){
              val += pow((temp%10),2);
-             count++;
              temp = temp/10;
          }  
          if(v.find(val)!=v.end()){return false;}
