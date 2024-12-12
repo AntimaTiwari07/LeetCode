722dc6c8efb3da81045c24ff7f6d9d605ff0d356
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-12 23:37:11 +0530

    Time: 0 ms (100%), Space: 13.1 MB (78.35%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         long long remgifts = 0;
+        /*
         int second = k;
         int maxnum = INT_MIN;
        for(int i = 0;i<second;i++){
@@ -18,6 +19,23 @@ public:
        for(int i = 0;i<gifts.size();i++){
         cout<<gifts[i]<<" ";
         remgifts+=gifts[i];
+       }
+       return remgifts;
+       */
+       priority_queue<int>v;
+       int second = k;
+       for(int i = 0;i<gifts.size();i++){
+        v.push(gifts[i]);
+       }
+       for(int i = 1;i<=second;i++){
+        int top = v.top();
+        v.pop();
+            v.push(sqrt(top));
+       }
+       while(!v.empty()){
+        remgifts+=v.top();
+        v.pop();
+        
        }
        return remgifts;
     }
