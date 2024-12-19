80b28bab60303b14c1c5c1f90bc88d487072265b
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-19 23:56:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (45.45%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -6,30 +6,31 @@ public:
      int carry = 0;
      string newstr="";
      while(i>=0 && j>=0){
-        int temp = num2[i]-'0' + num1[j]-'0'+carry;
+        int temp = num2[i]-48 + num1[j]-48 +carry;
         int x = temp%10;
          carry = temp/10;
-        newstr+=to_string(x);
+        newstr+=(char)(x+48);
         i--;
         j--;
      }
-     cout<<"c = "<<carry<<endl;
     while(i>=0){
-        int temp = num2[i]-'0'+carry;
+        int temp = num2[i]-48+carry;
         int x = temp%10;
-        newstr+=to_string(x);
+        newstr+=(char)(x+48);
         carry = temp/10;
         i--;
     }
     while(j>=0){
-         int temp = num1[j]-'0'+carry;
+         int temp = num1[j]-48+carry;
         int x = temp%10;
-        newstr+=to_string(x);
+        newstr+=(char)(x+48);
         carry = temp/10;
         j--;
     }
-    if(carry!=0){newstr+=to_string(carry);}
-    reverse(newstr.begin(),newstr.end());
+    if(carry!=0){newstr+=(char)(carry+48);}
+    for(int i = 0;i<newstr.size()/2;i++){
+        swap(newstr[i] , newstr[newstr.size()-i-1]);
+    }
     return newstr;
     }
 };
