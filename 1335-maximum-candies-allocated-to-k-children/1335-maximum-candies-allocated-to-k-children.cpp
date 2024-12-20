d935a556ec66243f620b3bded48bc4a0848d964c
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-21 02:49:07 +0530

    Time: 68 ms (5.47%), Space: 88.2 MB (26.72%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        if(candies.size()==1&&candies[0]==k) return 1;
+        if(candies.size()==1 && candies[0]>=k) return candies[0]/k;
+        
+        
+     sort(candies.begin(),candies.end());
+     int high = candies[candies.size()-1];
+     int low = 1;
+     int mid;
+     int ans = 0;
+     while(low<=high){
+        mid = low+(high-low)/2;
+        long long piles = 0;
+       
+        for(int i = 0;i<candies.size();i++){
+            piles+=candies[i]/mid;
+        }
+        if(piles>=k){
+         ans = mid;
+          cout<<ans<<endl;
+         low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+       
+     } 
+     return ans;
+    }
+};
