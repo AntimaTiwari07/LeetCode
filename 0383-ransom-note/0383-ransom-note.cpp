c7a3aaad07ca6673b8d1a2ae808b1969f100c474
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-21 01:32:11 +0530

    Time: 7 ms (46.8%), Space: 11.8 MB (30.89%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count = 0;
+      for(int i = 0;i<ransomNote.size();i++){
+        for(int j = 0;j<magazine.size();j++){
+           if(ransomNote[i]==magazine[j]){
+            count++;
+            magazine.erase(magazine.begin()+j);
+            break;
+           }
+        }
+      }
+      cout<<count<<endl;
+    if(count==ransomNote.size()) return true;
+    return false;  
+    }
+};
