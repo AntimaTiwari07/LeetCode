0b2cf79cc8ff3cda84c6907ed84b74ed766fad3c
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-20 09:09:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.11%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            int max = arr[i];
-            for(int j = i;j<=max;j++){
-                if(arr[j]>max) {
-                    max = arr[j];
-                }
-                if(j == max){
-                    i = max;
-                    ans++;
-                    break;
-                }
-            }
+        int maxpos = INT_MIN;
+        int chunks = 0;
+      for(int i = 0;i<arr.size();i++){
+        maxpos = max(maxpos,arr[i]);
+        if(maxpos==i) {
+            chunks++;
+                   }
+      }
+      return chunks;
         }
-        return ans;
-    }
 };
