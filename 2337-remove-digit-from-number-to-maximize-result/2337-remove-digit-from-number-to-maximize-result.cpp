47414bb11e41603e79fcc6c0371825a2c1522846
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-21 00:28:49 +0530

    Time: 39 ms (8.98%), Space: 27.2 MB (5.42%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.cpp
@@ -11,46 +11,8 @@ public:
                     }
             }
              v.push_back(newstr);
+}  
         }
-       
-        }
-        for(int i = 0;i<v.size();i++){
-            cout<<v[i]<<" ";
-        }
-        /*
-        for(int j = 0;j<v.size();j++){
-        for(int i = 0;i<v.size()-1;i++){
-            if(v[i]==v[i+1]){
-               v.erase(v.begin()+i);
-            }
-        }
-        }
-       int maxnum ;
-       int idx = 0;
-       int size = v[0].size();
-       cout<<"size = "<<size<<endl;
-             for(int j=0;j<size;j++){
-                 maxnum = v[0][j]-48;
-                 idx = 0;
-                 bool run = false;
-                for(int i = 1;i<v.size();i++){
-                     if( maxnum<v[i][j]-48){
-                        run = true;
-                        maxnum = v[i][j]-48;
-                        idx = i;
-                     }
-                     else if(maxnum==v[i][j]-48) {
-                        run = true;
-                        idx = 0;
-                }
-                }
-                 if(!run) break;
-              else if(idx!=0){
-                break;
-              }
-             }
-             return v[idx];
-             */
              string result = v[0];
              for(int i = 1;i<v.size();i++){
                 if(v[i]>result){
