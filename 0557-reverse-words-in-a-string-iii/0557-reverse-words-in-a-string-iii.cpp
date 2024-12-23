86b82b2cf21fa2601ec17ad02af9ffc1dd8c410f
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-24 01:08:17 +0530

    Time: 136 ms (5.01%), Space: 274 MB (5.01%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string newstr = "";
+        vector<string>v;
+        int st =0;
+        int end = 0;
+        string str = "";
+        while(end<s.size()){
+            if(s[end]==' '||end == s.size()-1){
+                str = "";
+                if(end==s.size()-1){
+                    end = s.size();
+                }
+                for(int idx = st;idx<end;idx++){
+                 str+=s[idx];
+                }
+                cout<<str<<endl;
+                v.push_back(str);
+                st = end+1;
+            }
+            end++;
+        }
+        
+        for(int i = 0;i<v.size();i++){
+            string st = v[i];
+           for(int j = 0;j<st.size()/2;j++){
+            swap(st[j],st[st.size()-1-j]);
+           }
+        if(i==v.size()-1){newstr = newstr+st;}
+           else newstr = newstr+st+" ";
+        
+        }
+        return newstr;
+    }
+};
