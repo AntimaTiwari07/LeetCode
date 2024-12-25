9d71ae1352a1241ec7584f58241a7a00916ecdc2
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2024-12-25 23:43:08 +0530

    Time: 219 ms (54.02%), Space: 25.1 MB (78.58%) - LeetHub

--- 3649-minimum-time-to-break-locks-i/3649-minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/3649-minimum-time-to-break-locks-i.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-int find_min_time(vector<int>&strength,int k){
-        int x = 1;
+    int findMinimumTime(vector<int>& strength, int k) {
+        int min_time = INT_MAX;
+        sort(strength.begin(),strength.end());
+        do{
+           int x = 1;
         int time = 0;
        for(int i = 0;i<strength.size();i++){
         if(strength[i]%x==0)time = time + strength[i]/x;
         else time = time + strength[i]/x+1;
         x +=k;
        }
-        return time;
-}
-    int findMinimumTime(vector<int>& strength, int k) {
-        int min_time = INT_MAX;
-        sort(strength.begin(),strength.end());
-        do{
-            int currtime = find_min_time(strength, k);
-            min_time = min(min_time,currtime);
+       min_time = min(min_time,time);
         }while(next_permutation(strength.begin(),strength.end()));
         return min_time;
     }
