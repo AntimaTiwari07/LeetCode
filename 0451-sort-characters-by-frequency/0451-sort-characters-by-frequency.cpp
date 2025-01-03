c2dfac5189abc595fcc8a56684c4c2a150acd726
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-01-03 23:09:31 +0530

    Time: 11 ms (9.11%), Space: 11 MB (63.09%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        /*
         int count[256]={0};
         for(int i = 0;i<s.size();i++){
          if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z' )||(s[i]>='0'&&s[i]<='9'))count[s[i]-'0']++;
@@ -21,6 +23,28 @@ public:
         }
         count[idx]=0;
         k++;
+        }
+        return newstr;
+        */
+        for(int i = 0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int k = 0;
+        string newstr="";
+        while(k<s.size()){
+            int maxi = INT_MIN;
+            char ele = '\0';
+            for(auto i:mp){
+                if(i.second>maxi){
+                    maxi=i.second;
+                    ele = i.first;
+                }
+            }
+            for(int i = 1;i<=maxi;i++){
+                newstr+=ele;
+            }
+            mp[ele]=0;
+            k++;
         }
         return newstr;
     }
