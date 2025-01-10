2a7420e5a365af20e1d963c025bb31332e4216e1
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-01-10 20:09:42 +0530

    Time: 1 ms (50.2%), Space: 14.2 MB (44.8%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,24 +4,26 @@ public:
        int n = nums1.size();
        int m = nums2.size();
        set<int>s;
-       if(n<=m){
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(nums1[i]==nums2[j]) {
+       sort(nums1.begin(),nums1.end());
+       sort(nums2.begin(),nums2.end());
+        for(int i  = 0;i<n;i++){
+            int st = 0;
+            int end = m-1;
+            int mid ;
+            while(st<=end){
+                mid = st+(end-st)/2;
+                if(nums2[mid]==nums1[i]){
                     if(s.find(nums1[i])==s.end()) s.insert(nums1[i]);
+                    break;
+                }
+                else if(nums2[mid]<nums1[i]){
+                    st = mid+1;
+                }
+                else{
+                    end = mid-1;
                 }
             }
         }
-       }
-       else{
-         for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(nums2[i]==nums1[j]) {
-                    if(s.find(nums2[i])==s.end()) s.insert(nums2[i]);
-                }
-            }
-        }
-       }
        vector<int>v;
        for(auto i :s){
         v.push_back(i);
