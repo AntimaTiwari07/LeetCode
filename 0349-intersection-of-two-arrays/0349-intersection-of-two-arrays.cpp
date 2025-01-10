a037db968c43a565095f2d49d9ea876ab3d8a74f
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-01-10 20:12:43 +0530

    Time: 0 ms (100%), Space: 13.7 MB (82.26%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
        int n = nums1.size();
        int m = nums2.size();
-       set<int>s;
+        vector<int>v;
        sort(nums1.begin(),nums1.end());
        sort(nums2.begin(),nums2.end());
         for(int i  = 0;i<n;i++){
@@ -13,7 +13,7 @@ public:
             while(st<=end){
                 mid = st+(end-st)/2;
                 if(nums2[mid]==nums1[i]){
-                    if(s.find(nums1[i])==s.end()) s.insert(nums1[i]);
+                   v.push_back(nums1[i]);
                     break;
                 }
                 else if(nums2[mid]<nums1[i]){
@@ -24,10 +24,13 @@ public:
                 }
             }
         }
-       vector<int>v;
-       for(auto i :s){
-        v.push_back(i);
-       }
+      
+      sort(v.begin(),v.end());
+      for(int i = 0;i<v.size();i++){
+        for(int j = 0;j<v.size()-1;j++){
+            if(v[j]==v[j+1]) v.erase(v.begin()+j);
+        }
+      }
        return v;
     }
 };
