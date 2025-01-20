39ac8852a7f537975adc8688ff495d179a1d66c1
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-01-20 21:59:14 +0530

    Time: 91 ms (5.37%), Space: 64.2 MB (72.56%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+       if(nums1.size()%2==0 && nums2.size()%2==0){
+      return 0;
+       }
+       else{
+        int x = 0;
+        if(nums1.size()%2!=0 &&nums2.size()%2==0){
+            for(int i = 0;i<nums2.size();i++){
+            x^=nums2[i];
+        }
+        for(int j = 0;j<nums1.size();j++){
+          ans^=x;
+        }
+       }
+       else if(nums1.size()%2==0 && nums2.size()%2!=0) {
+        int x = 0;
+            for(int i = 0;i<nums1.size();i++){
+            x^=nums1[i];
+        }
+        for(int j = 0;j<nums2.size();j++){
+          ans^=x;
+        }
+       }
+       else{
+         int x = 0;
+            for(int i = 0;i<nums1.size();i++){
+            for(int j = 0;j<nums2.size();j++){
+                ans^=(nums1[i]^nums2[j]);
+        }
+        }
+       }
+       }
+       return ans;
+    }
+};
