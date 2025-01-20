c6b43727ec352df671f00ba3b1c685afcb2582ad
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-01-20 22:01:42 +0530

    Time: 0 ms (100%), Space: 64.4 MB (43.16%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -26,10 +26,11 @@ public:
        }
        else{
          int x = 0;
-            for(int i = 0;i<nums1.size();i++){
             for(int j = 0;j<nums2.size();j++){
-                ans^=(nums1[i]^nums2[j]);
+                x^=nums2[j];
         }
+        for(int i = 0;i<nums1.size();i++){
+             ans^=(nums1[i]^x);
         }
        }
        }
