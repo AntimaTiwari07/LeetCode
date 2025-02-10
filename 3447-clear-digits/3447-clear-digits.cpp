224f1658182de85d1a6e6a1dbde8a0d38ddff1ac
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-02-10 17:39:18 +0530

    Time: 3 ms (15.28%), Space: 8.5 MB (70.42%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -3,11 +3,13 @@ public:
     string clearDigits(string s) {
         for(int i = 0;i<s.size();i++){
             if(s[i]>='0' && s[i]<='9'){
-                for(int j = i;j>=0;j--){
+                int j = i-1;
+               while(j>=0){
                    if(s[j]>='a' && s[j]<='z'){
                     s.erase(s.begin()+j);
                     break;
                    }
+                   j--;
                 }
                 s.erase(s.begin()+(i-1));
                 i-=2;
