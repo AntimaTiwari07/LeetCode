28fa42ee2b00bf057952db1e3b9804742925a182
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-02-11 21:56:39 +0530

    Time: 208 ms (5.99%), Space: 22.9 MB (46.54%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int>ans(ratings.size(),1);
+        //left
+        for(int i =1;i<ratings.size();i++){
+            if(ratings[i]>ratings[i-1]){
+                ans[i]=ans[i-1]+1;
+            }
+        }
+        //right
+        for(int i = ratings.size()-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                if(ans[i]<=ans[i+1])ans[i]=ans[i+1]+1;
+            }
+        }
+        int sum = 0;
+        for(int i = 0;i<ans.size();i++){
+            sum+=ans[i];
+            cout<<ans[i]<<" ";
+        }
+        return sum;
+    }
+};
