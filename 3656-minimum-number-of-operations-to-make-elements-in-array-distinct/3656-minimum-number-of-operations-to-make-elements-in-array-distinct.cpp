1d152c15854ab053117ed62c09346e441e43619d
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-01 00:23:44 +0530

    Time: 419 ms (5.12%), Space: 27 MB (96.72%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        vector<bool>v(101,false);
+        int op= 0;
+        for(int i =0;i<nums.size();i++){
+            bool valid= false;
+           for(int j =0;j<nums.size();j++){
+            if(!v[nums[j]]){
+                v[nums[j]] = true;
+                valid = true;
+            }
+            else{
+                 op+=1;
+                 cout<<"op = "<<op<<endl;
+                if(nums.size()>=3)
+                nums.erase(nums.begin(),nums.begin()+3);
+                else{
+                nums.erase(nums.begin(),nums.begin()+nums.size());
+                }
+                valid = false;
+                  break;
+           }
+        }
+        if(valid)return op;
+        i--;
+        for(int i = 0;i<nums.size();i++)cout<<nums[i]<<" ";
+        cout<<endl;
+        for(int i = 0;i<101;i++){
+            v[i]=false;
+        }
+        }
+        return op;
+    }
+};
