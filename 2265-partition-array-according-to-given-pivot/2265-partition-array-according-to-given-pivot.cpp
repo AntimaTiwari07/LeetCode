d98e27db4be6b4ef3c0f102d36ed0e254e5ede1e
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-06 10:56:41 +0530

    Time: 1735 ms (5.14%), Space: 127.6 MB (83.04%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+       vector<int>v(nums.size());
+       int cnt =0;
+       int dup = 0;
+       for(int i = 0;i<nums.size();i++){
+        if(nums[i]<pivot)cnt+=1;
+        else if(nums[i]==pivot)dup+=1;
+       } 
+       cout<<cnt<<endl;
+       int k = cnt+(dup-1)+1;
+       int s = 0;
+       for(int i = 0;i<nums.size();i++){
+         if(nums[i]<pivot){v[s]=nums[i];s++;}
+         else if(nums[i]>pivot){
+            v[k]=nums[i];
+            k++;
+         }
+       }
+       for(int i =0;i<dup;i++){
+        v[cnt] = pivot;
+        cnt+=1;
+       }
+       for(int i= 0;i<v.size();i++){
+        cout<<v[i]<<" ";
+       }
+       return v;
+    }
+};
