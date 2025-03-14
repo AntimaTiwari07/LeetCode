f9474c360153a2665f48920bfcbd38e54628f9ca
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-14 23:05:24 +0530

    Time: 62 ms (6.85%), Space: 88.1 MB (73.33%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         if(candies.size()==1&&candies[0]==k) return 1;
-        if(candies.size()==1 && candies[0]>=k) return candies[0]/k;
-        
-        
+        if(candies.size()==1 && candies[0]>=k) return candies[0]/k;          
      sort(candies.begin(),candies.end());
      int high = candies[candies.size()-1];
      int low = 1;
