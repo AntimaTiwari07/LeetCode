44fcc578dea7d7b9d78b6f6607db23340230f4d8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-22 23:38:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (34.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-long long fact(int n,int r){
+   vector<int>rowgenerate(int row){
+    vector<int>ans(row+1);
     long long res = 1;
-    for(int i = 0;i<r;i++){
-        res = res*(n-i);
-        res/=(i+1);
+    ans[0]=1;
+    for(int j = 1;j<=row;j++){
+        res=res*(row-j+1);
+        res = res/(j);
+        ans[j]=res;
     }
-    return res;
-}
+    return ans;
+   }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>v(numRows);
-        for(int i=0;i<numRows;i++){
-           for(int j = 0;j<=i;j++){
-            v[i].push_back(fact(i,j));
-           }
-        }
-        return v;
+        vector<vector<int>>traingle;
+       for(int i = 0;i<numRows;i++){
+            traingle.push_back(rowgenerate(i));
+       }
+       return traingle;
     }
 };
