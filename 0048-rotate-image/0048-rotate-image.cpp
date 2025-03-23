0c61c72573a1c0965d8b7e0c5e6e872be10c4931
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-23 20:46:25 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -12,10 +12,8 @@ public:
         }
         for(int i = 0;i<v.size();i++){
             for(int j = 0;j<v[i].size();j++){
-                cout<<v[i][j]<<" ";
                 matrix[i][j]=v[i][j];
             }
-            cout<<endl;
         }
     }
 };
