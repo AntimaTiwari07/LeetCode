3f9506007ccda17ca81da91b0c474049f34ad0a3
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-03-25 00:49:00 +0530

    Time: 0 ms (100%), Space: 9.2 MB (57.58%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -5,15 +5,14 @@ public:
         long num = 0;
         int sign=1 ;
         bool valid = false;
+        //whitespace
         while(i<s.size() && s[i]==' '){i++;}
-        
-            if(s[i]==' ')i++;
-
+        //sign
              if(i<s.size() &&(s[i]=='+' || s[i]=='-')){
                 sign = (s[i]=='-')?-1:1;
                 i++;
              }
-           
+           //numbers
                 while(i<s.size() && (s[i]>='0' && s[i]<='9')){
                     if(num > (INT_MAX - (s[i]-'0'))/10){
                       return (sign == 1) ? INT_MAX : INT_MIN;
