5f5ba9cb70b819060d7fb26d2c25100aacc81a9b
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-04-11 02:46:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (62.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,29 +1,59 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size()==1)return false;
-      stack<char>st;
+        /*
+       int count_open_bracket = 0;
+       int count_open_braces = 0;
+       int count_open_paranthesis = 0;
+       
       for(int i = 0;i<s.size();i++){
-        if(s[i]=='(' || s[i]=='{'||s[i]=='['){
-            st.push(s[i]);
+        if(s[i]=='('){
+            count_open_paranthesis++;
+        }
+        else if(s[i]=='['){
+           count_open_bracket++;
+        }
+        else if(s[i]=='{'){
+             count_open_braces++;
         }
         else{
-            if(!st.empty() && (s[i]==')'&& st.top()=='(')){
-                st.pop();
+            if(s[i]==')'){
+                count_open_paranthesis--;
             }
-            else if(!st.empty() && (s[i]=='}'&& st.top()=='{')){
-                st.pop();
-            }
-            else if(!st.empty() && (s[i]==']'&& st.top()=='[')){
-                    st.pop();
+            else if(s[i]==']'){
+                 count_open_bracket--;
             }
             else{
-                return false;
+                count_open_braces--;
             }
         }
       }
-      if(st.size()==0)return true;
-      return false;  
-
+      if( count_open_braces == 0 && count_open_bracket==0 &&  count_open_paranthesis==0){
+        return true;
+      }
+      return false;
+*/
+  stack<char>st;
+  for(int i = 0;i<s.size();i++){
+    if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+        st.push(s[i]);
     }
+    else{
+        if(!st.empty()){
+       char top = st.top();
+       if((s[i]==')' && top == '(') ||(s[i]=='}' && top == '{') || (s[i]==']' && top =='[')){
+                  st.pop();
+       } 
+       else{
+        return false;
+       }
+    }
+    else{
+        return false;
+    }
+    }
+  }
+  if(st.empty()){return true;}
+  return false;
+  }
 };
