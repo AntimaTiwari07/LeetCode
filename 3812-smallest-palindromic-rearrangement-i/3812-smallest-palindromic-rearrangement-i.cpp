43be20839f1fb6ef36c79b5f9082d759c5b7f401
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-04-13 13:53:41 +0530

    Time: 78 ms (52.63%), Space: 78.7 MB (47.37%) - LeetHub

---
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int>count(26,0);
+        for(int i = 0;i<s.size();i++){
+          count[s[i]-'a']++;
+        }
+        char odd ;
+        bool op = false;
+       for(int i = 0;i<26;i++){
+        if(count[i]!=0 && count[i]%2!=0){
+            odd = (char)(i+'a');
+            op = true;
+        }
+       }
+       string ans = "";
+       for(int i = 0;i<26;i++){
+        if(count[i]!=0 ){
+            char ch = (char)(i+'a');
+            string temp = string(count[i]/2,ch);
+            cout<<temp<<endl;
+            ans+=temp;
+            count[i]=count[i]/2;
+        }
+       }
+       if(op)ans+=odd;
+        for(int i = 25;i>=0;i--){
+            if(count[i]!=0){
+                char ch = (char)(i+'a'); 
+            string p = string(count[i],ch);
+            ans+=p;
+            }
+        }
+        bool valid = true;
+        for(int i = 0;i<s.size()/2;i++){
+            if(s[i]!=s[s.size()-i-1]){
+                valid = false;
+            }
+        }
+        if(valid){
+            if(s[0]<ans[0])return s;
+        }
+        return ans;
+    }
+};
