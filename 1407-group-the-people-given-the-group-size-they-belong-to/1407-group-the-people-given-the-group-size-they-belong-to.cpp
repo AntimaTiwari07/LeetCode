ec299e9d120702a25313cad9fd5735da821ad4a8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-04-20 22:31:32 +0530

    Time: 6 ms (30.92%), Space: 17.5 MB (29.26%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-       unordered_map<int,int>mp;
-       for(int i = 0;i<groupSizes.size();i++){
-        mp[groupSizes[i]]++;
-       }
-       int size = 0;
-       for(auto i:mp){
-          size+=(i.second/i.first);
-       }
-       vector<vector<int>>ans(size);
-       int p = 0;
-       for(auto i:mp){
-        for(int j = 0;j<(i.second/i.first);j++){
-            for(int l = 0;ans[p].size()<i.first && l<groupSizes.size();l++){
-                if(groupSizes[l]==i.first){
-                    ans[p].push_back(l);
-                    groupSizes[l]=0;
-                }
-           }
-           p++;
+      vector<vector<int>>res;
+      int n = groupSizes.size();
+     vector<vector<int>>mp(n+1);
+      for(int i = 0;i<groupSizes.size();i++){
+        int x = groupSizes[i];
+        mp[x].push_back(i);
+        if(mp[x].size()== x){
+            res.push_back(mp[x]);
+            mp[x] = {};
         }
-       }
-       return ans;
-
+      }
+     return res;
     }
 };
