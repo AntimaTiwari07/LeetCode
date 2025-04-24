dbe8d0352f639075d029932786e41b0f2c1aa992
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-04-24 22:59:16 +0530

    Time: 97 ms (23.96%), Space: 50.5 MB (24.77%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -7,13 +7,28 @@ public:
         }
         int dis_ele = distinct.size();
         int cnt = 0;
-        for(int i=0;i<nums.size();i++){
-            unordered_map<int,int>mp;
-            for(int j = i;j<nums.size();j++){
-                mp[nums[j]]++;
-                if(mp.size()==dis_ele)cnt+=1;
+        int i =0;
+        int j = 0;
+        int n = nums.size();
+        vector<int>v(2001,0);
+        while(i<n && j<n){
+            v[nums[j]]++;
+            int count  =0;
+            for(int k = 0;k<2001;k++){
+                if(v[k]>0)count+=1;
             }
+            while(count==dis_ele){
+                cnt+=(n-j);
+                v[nums[i]]--;
+                i++;
+                count =0;
+                 for(int k = 0;k<2001;k++){
+                if(v[k]>0)count+=1;
+            }
+            }
+             j++;
         }
+        
    return cnt;
     }
 };
