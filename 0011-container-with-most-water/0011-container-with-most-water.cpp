88e909f62b9df16d29825eac921df184222f78d8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-04-25 23:29:11 +0530

    Time: 2 ms (40.04%), Space: 62.9 MB (78.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,41 +5,17 @@ public:
         int maxsize = INT_MIN;
         int i = 0;
         int j = n-1;
-        unordered_map<int,int>mp;
         while(i<j){
             if(height[i]<height[j]){
-                mp[height[i]] = max(mp[height[i]],height[i]*(j-i));
+               maxsize = max(maxsize,height[i]*(j-i));
                 i++;
             }
             else{
-                 mp[height[j]] = max(mp[height[j]],height[j]*(j-i));
+                 maxsize = max(maxsize,height[j]*(j-i));
                  j--;
             }
         }
-
-    //    unordered_map<int,int>mp1;
-    //    i =0;
-    //    j = n-1;
-    //     while(i<j){
-    //         if(height[i]<=height[j]){
-    //             mp1[height[i]] = max(mp[height[i]],height[i]*(j-i));
-    //             i++;
-    //         }
-    //         else{
-    //              mp1[height[j]] = max(mp[height[j]],height[j]*(j-i));
-    //              j--;
-    //         }
-    //     }
-
-        for(auto i:mp){
-            // if(i.second == mp1[i.first]){
-            //     maxsize = max(maxsize,i.second);
-            // }
-            // else{
-            //     maxsize = max(maxsize,i.second+mp1[i.first]);
-            // }
-            maxsize = max(maxsize,i.second);
-        }
+     
         return maxsize;
     }
 };
