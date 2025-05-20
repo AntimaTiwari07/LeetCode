6b8f36457b1d1996ad0263c4cca968f5e224fa69
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-05-20 23:03:06 +0530

    Time: 10 ms (52.4%), Space: 292.6 MB (96.48%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,25 +1,20 @@
-
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-       //youtube
-       //for prefix sum
-       int n = nums.size()+1;
-       vector<int>prefixsum(n,0);
-       //-1 = start  1 = end
-       for(int i = 0;i<queries.size();i++){
-        prefixsum[queries[i][0]]=prefixsum[queries[i][0]]+ (-1);
-        prefixsum[queries[i][1]+1]= prefixsum[queries[i][1]+1]+1;
-       }
-       for(int i = 1;i<prefixsum.size();i++){
-           prefixsum[i]= prefixsum[i]+prefixsum[i-1];
-       }
-       for(int i = 0;i<nums.size();i++){
-        if(nums[i]!=0)nums[i]=nums[i]+prefixsum[i];
-       }
-       for(int i = 0;i<nums.size();i++){
-        if(nums[i]>0)return false;
-       }
-       return true;
+        vector<int>diff(nums.size()+1,0);
+        for(int i = 0;i<queries.size();i++){
+            diff[queries[i][0]]=diff[queries[i][0]]-1;
+            diff[queries[i][1]+1]=diff[queries[i][1]+1]+1;
+        }
+        for(int i =1;i<diff.size();i++){
+            diff[i]=diff[i]+diff[i-1];
+        }
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]+diff[i]<=0)nums[i]=0;
+        }
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]!=0)return false;
+        }
+        return true;
     }
 };
