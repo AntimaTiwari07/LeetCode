0807184a6a4e1cc20433e3c23c290f5ea7018bd2
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-06-13 23:46:44 +0530

    Time: 2 ms (54.29%), Space: 8 MB (37.26%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,20 +4,19 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-       int start = 1;
-       int end = n;
-       int mid ;
-       int ans =-1;
-       while(start<=end){
-        mid = start +(end-start)/2;
+      int st = 1;
+      int end = n;
+      int mid ;
+      int x ;
+      while(st<=end){
+        mid = st+(end-st)/2;
         if(isBadVersion(mid)){
-             ans = mid;
             end = mid-1;
         }
         else{
-            start = mid+1;
+             st = mid+1;
         }
-       } 
-       return ans;
+      }  
+      return st;
     }
 };
