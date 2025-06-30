02618aa084590061122d116cec53096ec3b78089
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-06-30 10:00:46 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.76%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,35 +11,18 @@
  */
 class Solution {
 public:
-void traverse(TreeNode* p,vector<int>&pp){
-    if(p==NULL ){
-        pp.push_back(10001);
-        return ;
-    }
-    pp.push_back(p->val);
-    traverse(p->left,pp);
-    traverse(p->right,pp);
-} 
-void traverse2(TreeNode* q,vector<int>&qq){
-    if(q==NULL){
-        qq.push_back(10001);
-        return;
+bool solve(TreeNode* p, TreeNode* q){
+    if(p==NULL && q==NULL)return true;
+    if(p==NULL && q!=NULL)return false;
+    if(p!=NULL && q==NULL)return false;
+   if(p->val!=q->val)return false;
+   bool l = solve(p->left,q->left);
+   if(l==false)return false;
+   bool r = solve(p->right,q->right);
+   if(r==false)return false;
+   return true;
 }
-    qq.push_back(q->val);
-    traverse2(q->left,qq);
-    traverse2(q->right,qq);
-}
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int>pp;
-        vector<int>qq;
-        traverse(p,pp);
-        traverse2(q,qq);
-        if(pp.size()!=qq.size())return false;
-        for(int i = 0;i<pp.size();i++){
-            
-            if(pp[i]!=qq[i])return false;
-        }
-        return true;
-    } 
+        return solve(p,q);
+    }
 };
