154ff0d115575ec442a773159390ef3ca443bc54
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-07-01 23:28:01 +0530

    Time: 3 ms (36.41%), Space: 9 MB (10.92%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count = 0;
+     for(int i = 1;i<word.size();i++){
+        if(word[i]==word[i-1]){
+            count+=1;
+            word.erase(word.begin()+i);
+            i--;
+        }
+     }
+     cout<<word<<endl;
+     return count+1;
+    }
+};
