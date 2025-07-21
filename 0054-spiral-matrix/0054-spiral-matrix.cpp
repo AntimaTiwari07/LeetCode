a9ed07610f94e7776f07a07c15a324461ad5a11f
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-07-22 00:12:08 +0530

    Time: 2 ms (1.78%), Space: 9.4 MB (12.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,41 +2,47 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>v;
-      int row1 = 0;
-      int row2 = matrix.size()-1;
-      int col1 = 0;
-      int col2 = matrix[0].size()-1;
-      while(((col1 < matrix[0].size())  && (row1 < matrix.size()) ) && (row2 >=0) &&(col2>=0)){
-      //row 1
-      if(col1<=col2){
-       for(int i = col1;i<=col2;i++)   v.push_back(matrix[row1][i]);
-      }
-      else break;
-      //last col
-      if((row1+1)<=row2){
-     for(int i = row1+1;i<=row2;i++)  v.push_back(matrix[i][col2]);
-      }
-      else break;
-      //last row
-      if((col2-1)>=col1){
-     for(int i = col2-1;i>=col1;i--) v.push_back(matrix[row2][i]);
-      }
-      else break;
-   
-     //first col
-      if((row2-1)>row1){
-     for(int i = row2-1;i>row1;i--) v.push_back(matrix[i][col1]);
-      }
-      else break;
-    
-      row1 ++;
-      col1++;
-       col2--;
-        row2--;
-    }
-    for(int i = 0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }
-    return v;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0;
+        int right = n-1;
+        int up = 0;
+        int bottom = m-1;
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        while(v.size()!=m*n){
+           //right
+           for(int i = left;i<=right;i++){
+            if(visited[up][i]==0){
+                v.push_back(matrix[up][i]);
+                visited[up][i] = 1;
+            }
+           }
+           //down
+           for(int i = up;i<=bottom;i++){
+            if(visited[i][right]==0){
+                v.push_back(matrix[i][right]);
+                visited[i][right] = 1;
+            }
+           }
+           //left
+           for(int i = right;i>=left;i--){
+            if(visited[bottom][i]==0){
+                v.push_back(matrix[bottom][i]);
+                visited[bottom][i] = 1;
+            }
+           }
+           //up
+           for(int i = bottom;i>=up;i--){
+            if(visited[i][left]==0){
+                v.push_back(matrix[i][left]);
+                visited[i][left] = 1;
+            }
+           }
+           left+=1;
+           right-=1;
+           up+=1;
+           bottom-=1;
+        }
+        return v;
     }
 };
