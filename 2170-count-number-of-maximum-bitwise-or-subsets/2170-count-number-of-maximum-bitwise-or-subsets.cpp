8f0691272407c2b49ba12371a717ad6f863baeef
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-02 23:26:46 +0530

    Time: 11 ms (59.07%), Space: 10.5 MB (97.07%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-void subset(vector<int>& nums,long long x,vector<int>&v,long long &maxi,int i){
+void subset(vector<int>& nums,long long x,long long &maxi,int &cnt,int i){
     if(i==nums.size()){
-        maxi = max(maxi,x);
-        v.push_back(x);
+       if(x==maxi)cnt+=1;
         return;
     }
-    subset(nums,(x|nums[i]),v,maxi,i+1);
-    subset(nums,x,v,maxi,i+1);
+    subset(nums,(x|nums[i]),maxi,cnt,i+1);
+    subset(nums,x,maxi,cnt,i+1);
 }
     int countMaxOrSubsets(vector<int>& nums) {
         long long maxi = 0;
-        vector<int>v;
-        subset(nums,0,v,maxi,0);
-        int cnt = 0;
-        for(int i = 0;i<v.size();i++){
-            if(v[i]==maxi)cnt+=1;
+        for(int i = 0;i<nums.size();i++){
+            maxi = (maxi | nums[i]);
         }
+        int cnt = 0;
+        subset(nums,0,maxi,cnt,0);
+        
         return cnt;
     }
 };
