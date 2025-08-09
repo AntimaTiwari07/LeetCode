54ba3b6494db15effe5ea4490a30c48e65eb6f1a
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-09 22:52:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.81%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-bool power(int num){
-    if(num==1) return true;
-    if(num<=0 || num%2!=0) return false;
-    return power(num/2);
-}
     bool isPowerOfTwo(int n) {
-       return power(n);
-
+        if(n==0)return false;
+        while(n!=1){
+            if(n%2!=0)return false;
+            n/=2;
+        }
+        return true;
     }
 };
