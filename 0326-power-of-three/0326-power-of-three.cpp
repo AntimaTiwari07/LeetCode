d56147185602d2557c2cfe6e3ae97a980b0bdbba
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-13 13:29:23 +0530

    Time: 7 ms (23.24%), Space: 8.9 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+bool solve(int n){
+    if(n==1)return true;
+    if(n%3!=0)return false;
+    int half = n/3;
+    return solve(half);
+}
     bool isPowerOfThree(int n) {
         if(n==0)return false;
-      while(n!=1){
-        if(n%3!=0)return false;
-        n/=3;
-      }  
-      return true;
+      return solve(n);
     }
 };
