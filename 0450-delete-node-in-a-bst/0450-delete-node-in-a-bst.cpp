0c32646c78d5e8d278c4c0109e3cc453f10b1e61
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-21 00:11:42 +0530

    Time: 8 ms (0.64%), Space: 39.9 MB (30.51%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,45 +11,39 @@
  */
 class Solution {
 public:
-    TreeNode*  deletenode(TreeNode* root, int key){
-    if(root==NULL)return NULL ;
-    //search the key node
-    if(root->val==key){
-        //0 child
-        if(root->left==NULL && root->right==NULL){delete root;return NULL;}
-        //1 child
 
-        //left child
-       else if(root->left!=NULL && root->right==NULL){
-            TreeNode* temp = root->left;
-            delete root;
-            return temp;
-        }
-        //right child
-        else if(root->left==NULL && root->right!=NULL){
-            TreeNode* temp = root->right;
-            delete root;
-            return temp;
-        }
-
-        else if(root->left!=NULL && root->right!=NULL) {
-        TreeNode * temp = root;
-        if( temp->right) temp = temp->right;
-        while(temp->left!=NULL){
-            temp = temp->left; 
-        }
-         root->val = temp->val;
-         root->right = deletenode(root->right,temp->val);
-         return root;
+TreeNode* insert(vector<int>&inorder,vector<int>&preorder,int st,int end,int &pre_idx){
+    if(st>end )return NULL;
+    int curr_val = preorder[pre_idx];
+    TreeNode* root = new TreeNode (curr_val) ;
+    pre_idx++;
+    int idx = -1;
+    for(int i = st;i<=end;i++){
+        if(inorder[i]==curr_val){
+           idx = i;
+           break;
         }
     }
-     if(root->val<key){
-        root->right = deletenode(root->right,key);
-    }
-       else root->left = deletenode(root->left,key);
-      return root;
+      root->left = insert(inorder,preorder,st,idx-1,pre_idx);
+             root->right = insert(inorder,preorder,idx+1,end,pre_idx);
+  return root;
+}
+void traverse(TreeNode* root,vector<int>&inorder,vector<int>&preorder,int &val,int &count){
+    if(root==NULL)return;
+    if(root->val!=val)preorder.push_back(root->val);
+    else count+=1;
+    traverse(root->left,inorder,preorder,val,count);
+    if(root->val!=val)inorder.push_back(root->val);
+    traverse(root->right,inorder,preorder,val,count);
 }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        return deletenode(root,key);      
+        vector<int>inorder;
+        vector<int>preorder;
+        int count = 0;
+        traverse(root,inorder,preorder,key,count);
+        cout<<"count = "<<count<<endl;
+        if(count==0)return root;
+        int idx = 0;
+      return insert(inorder,preorder,0,inorder.size()-1,idx);
     }
 };
