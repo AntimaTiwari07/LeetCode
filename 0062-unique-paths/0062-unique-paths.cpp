fcd6997321f3cf4cb99a70f8ad72d691a1e9aafb
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-23 23:19:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-long long fact(int x,int y){
-    long long mul = 1;
-   for(int i = 1;i<=y;i++){
-        mul = mul*(x-i+1);
-        mul/=i;
-   }
-   return mul;
-}
     int uniquePaths(int m, int n) {
-        m--;
-        n--;
-         if(m==0 && n==0)return 1;
-        if(m==0 || n==0)return 1;
-        
-       int y = min(m,n);
-        int ans = fact(m+n,y);
-     return ans; 
+        vector<vector<int>>v(m,vector<int>(n,0));
+        v[0][0] = 1;
+        int up = 0,left = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i-1>=0)up = v[i-1][j];
+                if(j-1>=0)left = v[i][j-1];
+                v[i][j] = v[i][j]+left+up;
+                up = 0;left = 0;
+            }
+        }
+        return v[m-1][n-1];
     }
 };
