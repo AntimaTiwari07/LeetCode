42de192173f2dcc5011c9bc704ebaf5d8c2cc957
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-08-30 11:32:00 +0530

    Time: 0 ms (100%), Space: 21.7 MB (86.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,112 +1,35 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        /*
-        int rowi = 0;
-        int rowf = 8-rowi;
-        int coli=0;
-        int colf = 8-coli;
-        while(rowi<=rowf && coli<=colf){
-            //to found and count element in row
-            int countrowi[10] = {0};
-            int countrowf[10] = {0};
-        for(int i = coli;i<=colf;i++){
-            if((board[rowi][i]<48)) continue;   
-            else{
-               countrowi[board[rowi][i] -'0']++; 
-            }   
-        }
-            //to find comman in final row
-            for(int i = coli;i<=colf;i++){
-            if((board[rowf][i]<48)) continue;   
-            else{
-               countrowf[board[rowf][i] -'0']++; 
-            }   
-        }
-             //to found and count element in col
-            int countcoli[10] = {0};
-            int countcolf[10] = {0};
-        for(int i = rowi;i<=rowf;i++){
-            if((board[i][coli]<48)) continue;   
-            else{
-               countcoli[board[i][coli] -'0']++; 
-            }   
-        }
-            //to find comman in final row
-            for(int i = rowi;i<=rowf;i++){
-            if((board[i][colf]<48)) continue;   
-            else{
-               countcolf[board[i][colf] -'0']++; 
-            }   
-        }
-        
-            //check duplicate in row
-            for(int i = 0;i<10;i++){
-                if(countrowi[i] == 2){
-                         valid = false;
-                    break;
-                }
-               else if(countrowf[i]==2){
-                    valid = false;
-                    break;
-                }
-                else if(countcoli[i]==2){
-                  valid = false;
-                    break;
-                }
-               else if(countcolf[i]==2){
-                    valid =false;
-                    break;
-                }
-            }
-        rowi++;
-            coli++;
-        }
-        */
-        int countr[10]={0};
-        int countc[10]={0};
-        int x = 0;//row & col
-        while(x<9){
-    //count row
-            for(int i = 0;i<9;i++){
-                if(board[x][i]<47) continue;
-               else  countr[board[x][i]-'0']++;
-            }
-              for(int j = 0;j<10;j++){
-            if(countr[j]>1){
-                return false;
-            }else
-                  countr[j]=0;
-            }
-     //count col;
-             for(int i = 0;i<9;i++){
-                if(board[i][x]=='.') continue;
-               else  countc[board[i][x]-'0']++;
-             }
-
-             for(int j = 0;j<10;j++){
-            if(countc[j]>1){
-                return false;
-            }else
-                 countc[j]=0;
-             }
-            x++;
-        }
-    //check grid
-    std:: unordered_set<char>seen;
-    for(int p = 0;p<9;p+=3){
-        for(int k = 0;k<9;k+=3){
-            for(int i = p;i<p+3;i++){
-               for(int j = k;j<k+3;j++){
-                  if(board[i][j] != '.' && seen.find(board[i][j])!=seen.end()){
-                    return false;
-                  }
-                  else seen.insert(board[i][j]);
-               }
-            }
-            seen.erase(seen.begin(),seen.end());
+bool valid(vector<vector<char>>& board,int row,int col,char val){
+   for(int i = 0;i<9;i++){
+    if(i!=col){
+        if(board[row][i]==val)return false;
+    }
+   }
+   for(int i = 0;i<9;i++){
+    if(i!=row){
+        if(board[i][col]==val)return false;
+    }
+   }
+   int startrow = (row/3)*3;
+   int startcol = (col/3)*3;
+   for(int i = startrow;i<startrow+3;i++){
+    for(int j = startcol;j<startcol+3;j++){
+        if(i!=row && j!=col){
+            if(board[i][j]==val)return false;
         }
     }
-    return true;     
+   }
+   return true;
+}
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;j++){
+                if(board[i][j]!='.'){
+                if(!valid(board,i,j,board[i][j])) return false;
+                }            
+            }
+        }
+        return true;
     }
 };
