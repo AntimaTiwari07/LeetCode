cab6087404ce9717e543c687ce7e80f9f527e87d
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-09-01 17:53:12 +0530

    Time: 0 ms (100%), Space: 18.2 MB (5.6%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,15 +6,13 @@ public:
         if(nums.size()==1)return nums[0];
         for(int i = 0;i<nums.size();i++){
          if(nums[i]==0 && !v.empty()){
-            cout<<"size = "<<v.size()<<endl;
             int lastnum = v[v.size()-1];
              maxprod = max({maxprod,lastnum,0});
 
             for(int j = 0;j<=(int)v.size()-2;j++){
             maxprod = max({maxprod,v[j],lastnum/v[j]});
             }
-             
-              cout<<lastnum<<endl;
+            
             v.erase(v.begin(),v.end());
          }
          else{
@@ -29,11 +27,9 @@ public:
              maxprod = max({maxprod,lastnum});
              if(v.size()>=2){
             for(int j = 0;j<=(int)v.size()-2;j++){
-                cout<<v[j]<<" ";
             maxprod = max({maxprod,v[j],lastnum/v[j]});
             }
              }
-            cout<<lastnum<<endl;
         return maxprod;
     }
 };
