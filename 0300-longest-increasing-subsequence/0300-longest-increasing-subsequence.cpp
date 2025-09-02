2d4ae91488641d2229fae6f7834adcae0ac54f46
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-09-02 23:59:27 +0530

    Time: 478 ms (28.29%), Space: 296.8 MB (6.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-int solve(vector<int>&nums,int prev_idx,int idx,vector<vector<int>>&dp){
-    if(idx==nums.size()){
-        return 0;
-    }
-    if(dp[idx][prev_idx+1]!=-1)return dp[idx][prev_idx+1];
-    int not_take = solve(nums,prev_idx,idx+1,dp);
-    int take;
-    if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
-     take = 1+ solve(nums,idx,idx+1,dp);
-    }
-    return dp[idx][prev_idx+1] = max(take,not_take);
+int solve(vector<int>& nums,int prev_idx,int curr_idx,vector<vector<int>>&dp){
+    if(curr_idx==nums.size())return 0;
+     if(dp[prev_idx+1][curr_idx]!=-1)return dp[prev_idx+1][curr_idx];
+    int take =0;
+    if(prev_idx==-1 || nums[curr_idx]>nums[prev_idx]) take = 1+solve(nums,curr_idx,curr_idx+1,dp);
+    int not_take = solve(nums,prev_idx,curr_idx+1,dp);
+    return dp[prev_idx+1][curr_idx]=  max(take,not_take);
 }
     int lengthOfLIS(vector<int>& nums) {
-       vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
         return solve(nums,-1,0,dp);
     }
 };
