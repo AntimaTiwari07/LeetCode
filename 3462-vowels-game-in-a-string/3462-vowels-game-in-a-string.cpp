a301a72911d4424ff97147e491d5a5dfe69027c8
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-09-12 22:50:34 +0530

    Time: 14 ms (21.08%), Space: 18.9 MB (95.71%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int cnt = 0;
+        int idx = -1;
+        for(int i = 0;i<s.size();i++){
+            if(s[i]=='a' || s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u'){
+                cnt+=1;
+            }
+            if(cnt%2!=0){
+                idx = i;
+            }
+        }
+        cout<<idx<<endl;
+        if(idx==-1)return false;
+        if(idx==s.size())return true;
+        int con = 0;
+        int vow = 0;
+        for(int i = idx+1;i<s.size();i++){
+            if(s[i]!='a' && s[i]!='e'&& s[i]!='i'&& s[i]!='o'&& s[i]!='u')con+=1;
+            else vow+=1;
+        }
+        if(vow==0 && con!=0)return false;
+        if(con==0 && vow!=0)return true;
+        return true;
+    }
+
+};
