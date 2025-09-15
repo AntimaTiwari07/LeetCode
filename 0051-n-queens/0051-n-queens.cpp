0864ff53a5a4e607e8864d8ea8994acad8c7e363
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-09-15 11:06:16 +0530

    Time: 4 ms (37.44%), Space: 10.2 MB (74.58%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+bool safe(vector<string>&v,int row,int col,int size){
+    //row marked
+     for(int i = 0;i<size;i++){
+        if(v[i][col]=='Q')return false;
+     }
+     //col marked
+     for(int i  =0;i<size;i++){
+        if(v[row][i]=='Q')return false;
+     }
+
+     //right up diagonal
+     int x = row;
+    int y = col;
+     while(x>=0 && y<size){
+        if(v[x][y]=='Q')return false;
+        x-=1;
+        y+=1;
+     }
+
+     //left up diagonal
+     x = row;
+     y = col;
+     while(x>=0 && y>=0){ 
+         if(v[x][y]=='Q')return false;
+        x-=1;
+        y-=1;
+     }
+
+     return true;
+}
+
+
+void solve(vector<string>&v,int n,int row,vector<vector<string>>&ans){
+if(row==n){
+    ans.push_back(v);
+    return;
+}
+    for(int i = 0;i<n;i++){
+        if(safe(v,row,i,n)){
+            v[row][i]='Q';
+            solve(v,n,row+1,ans);
+            v[row][i]='.'; //backtrack step
+        }
+    }
+}
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>v(n,string(n,'.'));
+      solve(v,n,0,ans);
+      return ans;
+    }
+};
