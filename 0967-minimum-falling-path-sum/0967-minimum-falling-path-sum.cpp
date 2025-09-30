2d51da421f0240c6c381cd4f4d0be9c24466d270
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-10-01 03:19:28 +0530

    Time: 0 ms (100%), Space: 15 MB (39.59%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& matrix,int row,int col,vector<vector<int>>&dp){
-    if(row<0 || row>=matrix.size() || col<0 || col>=matrix[0].size()) return 1e9;
-    if(row==matrix.size()-1)return matrix[row][col];
-    if(dp[row][col]!=-1)return dp[row][col];
-    int left = solve(matrix,row+1,col-1,dp);
-    int down = solve(matrix,row+1,col,dp);
-    int right = solve(matrix,row+1,col+1,dp);
-    return dp[row][col] = matrix[row][col]+min({left,right,down});
-}
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int ans = INT_MAX;
-        vector<vector<int>>dp(101,vector<int>(101,0));
         int m = matrix.size();
         int n = matrix[0].size();
+         vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i = 0;i<n;i++){
             dp[m-1][i] = matrix[m-1][i];
         }
@@ -23,10 +14,8 @@ int solve(vector<vector<int>>& matrix,int row,int col,vector<vector<int>>&dp){
                 if(col-1>=0) left = dp[row+1][col-1];
                 if(col<n) down = dp[row+1][col];
                 if(col+1<n) right = dp[row+1][col+1];
-                
            dp[row][col] = matrix[row][col]+min({left,right,down});
-            }
-       
+            }    
         }
        
         for(int i = 0;i<n;i++){
