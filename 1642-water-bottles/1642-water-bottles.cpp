e73636d13e47c1e4bb5c9930cc0a9c2c4f472013
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-10-01 19:48:03 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.58%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-       int count = 0;
-       int bottle = numBottles;
-       while(bottle>=numExchange){
-        int ans = bottle/numExchange;
-         count+=ans;
-        bottle = ans + bottle%numExchange;
-       }
-       return count+numBottles;
+        int sum = numBottles;
+        int div =numBottles ,rem = 0;
+        while(div!=0){
+            div = numBottles/numExchange;
+            rem = numBottles%numExchange;
+            sum+=div;
+            numBottles = div+rem;
+        }
+        return sum;
     }
 };
