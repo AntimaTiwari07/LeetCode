5bb0e53976ed7f6eed3fa94dc175bc08b2904f36
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-10-08 00:02:11 +0530

    Time: 147 ms (59.41%), Space: 139.7 MB (56.22%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//this solution is based on transition no , even means buy and odd means sell b,s,b,s
 int f(vector<int>& prices,int i,int tran,vector<vector<int>>&dp){
     if(i==prices.size())return 0;
     if(tran==4)return 0;
@@ -12,7 +13,21 @@ int f(vector<int>& prices,int i,int tran,vector<vector<int>>&dp){
     return dp[i][tran] = profit;
 }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size(),vector<int>(4,-1));
-        return f(prices,0,0,dp);
+        vector<vector<int>>dp(prices.size()+1,vector<int>(5,0));
+        int n = prices.size();
+        for(int tran = 0;tran<4;tran++){
+            dp[n][tran] = 0;
+        }
+        for(int i = n-1;i>=0;i--){
+            for(int tran = 3;tran>=0;tran--){
+              int profit = INT_MIN;
+    if(tran%2==0){
+        profit = max(-prices[i]+dp[i+1][tran+1],dp[i+1][tran]);
+    }
+    else profit = max(prices[i]+dp[i+1][tran+1],dp[i+1][tran]);
+    dp[i][tran] = profit;
+            }
+        }
+        return dp[0][0];
     }
 };
