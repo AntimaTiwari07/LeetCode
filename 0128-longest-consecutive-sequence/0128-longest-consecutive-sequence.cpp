610b5153aada68b8a849332bafb78ab4e3d0c258
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-10-22 02:16:10 +0530

    Time: 1726 ms (5.05%), Space: 91.1 MB (17.04%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int,int>mp;
+   for(int i = 0;i<nums.size();i++){
+    mp[nums[i]]++;
+   }
+   int cnt = 0;
+   int ans = INT_MIN;
+   int val ;
+   for(auto i:mp){
+    cout<<i.first<<" "<<i.second<<endl;
+    if(cnt==0){
+      val = i.first;
+      cnt+=1;
+    }
+    if(i.first-val==1){
+        cnt+=1;
+        val = i.first;
+    }
+    else{
+        ans = max(ans,cnt);
+        cnt = 1;
+        val = i.first;
+    }
+   }
+   ans = max(ans,cnt);
+   return ans;
+    }
+};
