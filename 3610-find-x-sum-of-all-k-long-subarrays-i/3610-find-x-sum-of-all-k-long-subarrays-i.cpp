d611c9466d6c65fe691ee48ebb565b89b21876af
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-04 23:30:32 +0530

    Time: 11 ms (77%), Space: 34.6 MB (52.4%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -2,55 +2,37 @@ class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         unordered_map<int,int>mp;
+        vector<int>v;
         for(int i = 0;i<k;i++){
             mp[nums[i]]++;
         }
-        int n = nums.size();
-        vector<int>v;
-         priority_queue<int>pq;
-         unordered_map<int,int>temp = mp;
-            for(auto i:mp){
-                pq.push(i.second);
-            }
-            int cnt = x;
-            int ans = 0;
-            while(cnt!=0){
-                int val = pq.top();
-                pq.pop();
-                int maxi = 0;
-                for(auto i:temp){
-                if(i.second==val)maxi = max(maxi,i.first);
-                }
-                temp[maxi]=0;
-                cnt-=1;
-                ans+= val*maxi;
-            }
-           v.push_back(ans);
-        for(int j = 1;j<=n-k;j++){
-            mp[nums[j-1]]--;
-            mp[nums[j+k-1]]++;
-            priority_queue<int>pq;
-           unordered_map<int,int>temp = mp;
-            for(auto i:mp){
-                pq.push(i.second);
-            }
-            int cnt = x;
-            int ans = 0;
-            while(cnt!=0){
-                int val = pq.top();
-                pq.pop();
-                int maxi = 0;
-                for(auto i:temp){
-                if(i.second==val)maxi = max(maxi,i.first);
-                }
-               
-                temp[maxi]=0;
-                cnt-=1;
-                ans+= val*maxi;
-            }
-            
-            v.push_back(ans);
-            
+        priority_queue<pair<int,int>>pq;
+        for(auto i:mp){
+            pq.push({i.second,i.first});
+        }
+        int ans = 0;
+        for(int j = 0;j<x && pq.size();j++){
+            pair<int,int>p = pq.top();
+            pq.pop();
+            ans+=(p.first*p.second);
+        }
+        v.push_back(ans);
+       int n = nums.size();
+        for(int i = 1;i<=n-k;i++){
+            mp[nums[i+k-1]]++;
+            mp[nums[i-1]]--;
+        priority_queue<pair<int,int>>pq;
+        for(auto i:mp){
+            pq.push({i.second,i.first});
+        }
+        int ans = 0;
+        for(int j = 0;j<x && pq.size();j++){
+            pair<int,int>p = pq.top();
+            pq.pop();
+            ans+=(p.first*p.second);
+        }
+        v.push_back(ans);
+
         }
         return v;
     }
