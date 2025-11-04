50042c59f6ec217f4718b6d74cb218765a8330e4
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-04 23:19:15 +0530

    Time: 18 ms (46.65%), Space: 40 MB (6.39%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
-        vector<int> ans;  // To store the results
-
-        // Iterate over each subarray of size 'k'
-        for (int i = 0; i <= nums.size() - k; i++) {
-            vector<vector<int>> freqVec(51, vector<int>(2, 0));  // 2D vector to store number and its frequency (size 51 because 1 <= nums[i] <= 50)
-
-            // Fill the frequency array for the current subarray of size 'k'
-            for (int j = i; j < i + k; j++) {
-                int num = nums[j];
-                freqVec[num][0] = num;  // First column stores the number
-                freqVec[num][1]++;      // Second column stores the frequency
-            }
-
-            // Remove numbers with frequency 0 and sort by frequency and value
-            vector<vector<int>> validFreqVec;
-            for (int j = 1; j <= 50; j++) {
-                if (freqVec[j][1] > 0) {
-                    validFreqVec.push_back(freqVec[j]);  // Only include numbers with frequency > 0
-                }
-            }
-
-            // Sort by frequency in descending order, if frequency is the same, prioritize larger number
-            sort(validFreqVec.begin(), validFreqVec.end(), [](vector<int>& a, vector<int>& b) {
-                if (a[1] == b[1]) return a[0] > b[0];  // If frequencies are equal, prioritize larger number
-                return a[1] > b[1];  // Otherwise, prioritize by frequency
-            });
-
-            // Calculate the x-sum of the top 'x' most frequent elements
-            int sum = 0;
-            for (int j = 0; j < min(x, (int)validFreqVec.size()); j++) {
-                sum += validFreqVec[j][0] * validFreqVec[j][1];  // Add number * its frequency
-            }
-
-            ans.push_back(sum);  // Store the sum in the answer array
+        unordered_map<int,int>mp;
+        for(int i = 0;i<k;i++){
+            mp[nums[i]]++;
         }
-
-        return ans;  // Return the final answer
+        int n = nums.size();
+        vector<int>v;
+         priority_queue<int>pq;
+         unordered_map<int,int>temp = mp;
+            for(auto i:mp){
+                pq.push(i.second);
+            }
+            int cnt = x;
+            int ans = 0;
+            while(cnt!=0){
+                int val = pq.top();
+                pq.pop();
+                int maxi = 0;
+                for(auto i:temp){
+                if(i.second==val)maxi = max(maxi,i.first);
+                }
+                temp[maxi]=0;
+                cnt-=1;
+                ans+= val*maxi;
+            }
+           v.push_back(ans);
+        for(int j = 1;j<=n-k;j++){
+            mp[nums[j-1]]--;
+            mp[nums[j+k-1]]++;
+            priority_queue<int>pq;
+           unordered_map<int,int>temp = mp;
+            for(auto i:mp){
+                pq.push(i.second);
+            }
+            int cnt = x;
+            int ans = 0;
+            while(cnt!=0){
+                int val = pq.top();
+                pq.pop();
+                int maxi = 0;
+                for(auto i:temp){
+                if(i.second==val)maxi = max(maxi,i.first);
+                }
+               
+                temp[maxi]=0;
+                cnt-=1;
+                ans+= val*maxi;
+            }
+            
+            v.push_back(ans);
+            
+        }
+        return v;
     }
 };
