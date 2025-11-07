3293628bf3933fb7982fa503ba1f41faddb59927
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-08 00:14:25 +0530

    Time: 4 ms (54.54%), Space: 64.9 MB (95.1%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-       vector<int>trusty(n+1,0);
-       vector<int>ontrust(n+1,0);
+       vector<int>trustcnt(n+1,0);
         for(int i = 0;i<trust.size();i++){
-            trusty[trust[i][0]]++;
-            ontrust[trust[i][1]]++;
+            trustcnt[trust[i][0]]--;
+            trustcnt[trust[i][1]]++;
         }
        for(int i = 1;i<=n;i++){
-        if(trusty[i]==0 && ontrust[i]==(n-1))return i;
+        if(trustcnt[i]==(n-1))return i;
        }
         return -1;
     }
