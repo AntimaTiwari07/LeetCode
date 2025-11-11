9847077333512faf16204ff7ef4db2c1d28ccbdc
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-11 20:36:58 +0530

    Time: 47 ms (36.93%), Space: 75.7 MB (32.8%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,46 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
- ListNode* merge(ListNode* a, ListNode* b) {
-        ListNode *c = new ListNode(-1);
-        ListNode* temp = c;
-        while(a!=NULL && b!=NULL){
-            if(a->val <= b->val){
-                temp->next = a;
-                a = a->next;//a ko aage bada do
-                temp = temp->next;//temp ko aage bada do
-            }
-            else{
-                 temp->next = b;
-                b = b->next;//a ko aage bada do
-                temp = temp->next;//temp ko aage bada do
-            }
+ListNode* merge(ListNode* a,ListNode* b){
+    ListNode * x = new ListNode(-1);
+    ListNode* curr = x;
+     while(a!=NULL && b!=NULL){
+        if(a->val<=b->val){
+            curr->next = a;
+            curr = curr->next;
+            a = a->next;
         }
-        if(a==NULL) temp->next = b;
-        else temp->next = a;    
-        return c->next;
-    }
-//sc = O(1)
-//tc = O(nlogn)
+        else{
+            curr->next = b;
+            curr = curr->next;
+            b = b->next;
+        }
+     }
+    
+    if(a==NULL)curr->next = b;
+    else curr->next = a;
+    return x->next;
+}
     ListNode* sortList(ListNode* head) {
-        //base case
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-        ListNode * slow = head;
-        ListNode *fast = head;
-        //left middle find out kia
-        while(fast->next!=NULL && fast->next->next!=NULL){
+        if(head==NULL || head->next==NULL)return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow->next!=NULL && fast->next!=NULL && fast->next->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
-        //now sloq at left middle
-        ListNode *a = head;//list 1   4,2
-        ListNode *b = slow->next;//list 2    1,3
+        ListNode *a = head;
+        ListNode *b = slow->next;
         slow->next = NULL;
-        //recursion
-        a = sortList(a);
-        b = sortList(b);
-        ListNode *c = merge(a,b);
+        ListNode* x = sortList(a);
+        ListNode* y = sortList(b);
+        ListNode *c = merge(x,y);
         return c;
     }
 };
