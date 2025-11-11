8c3470cd7fa14ded8a4bb4ffabdb85b60c712a13
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-11 22:42:15 +0530

    Time: 20 ms (54.91%), Space: 65.3 MB (47.24%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,40 +10,21 @@
  */
 class Solution {
 public:
-ListNode* merge(ListNode* a,ListNode* b){
-    ListNode * x = new ListNode(-1);
-    ListNode* curr = x;
-     while(a!=NULL && b!=NULL){
-        if(a->val<=b->val){
-            curr->next = a;
-            curr = curr->next;
-            a = a->next;
-        }
-        else{
-            curr->next = b;
-            curr = curr->next;
-            b = b->next;
-        }
-     }
-    
-    if(a==NULL)curr->next = b;
-    else curr->next = a;
-    return x->next;
-}
     ListNode* sortList(ListNode* head) {
-        if(head==NULL || head->next==NULL)return head;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(slow->next!=NULL && fast->next!=NULL && fast->next->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+        ListNode* curr = head;
+        vector<int>arr;
+        while(curr!=NULL){
+            arr.push_back(curr->val);
+            curr = curr->next;
         }
-        ListNode *a = head;
-        ListNode *b = slow->next;
-        slow->next = NULL;
-        ListNode* x = sortList(a);
-        ListNode* y = sortList(b);
-        ListNode *c = merge(x,y);
-        return c;
+        sort(arr.begin(),arr.end());
+        ListNode* x = new ListNode(-1);
+        ListNode* temp = x;
+        for(int i = 0;i<arr.size();i++){
+            ListNode* t = new ListNode(arr[i]);
+             temp->next = t;
+             temp = temp->next;
+        }
+        return x->next;
     }
 };
