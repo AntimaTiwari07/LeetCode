754663bf2bed9407edf39aeef2729aa54f189a0f
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-15 00:14:46 +0530

    Time: 0 ms (100%), Space: 16.4 MB (95.63%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,46 +10,47 @@
  */
 class Solution {
 public:
+ListNode* reverseList(ListNode* head){
+    ListNode* curr = head;
+    ListNode* prev = NULL;ListNode* Next = NULL;
+    while(curr!=NULL){
+        Next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+    }
+    return prev;
+}
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* curr = head;
-        ListNode* prev = NULL;
-        ListNode* Next = NULL;
-        int len = 0;
+        ListNode* temp = head;
+        ListNode* nextNode = NULL;
+        ListNode* prevNode = NULL;
+
+        int i = 1;
         while(curr!=NULL){
-            len+=1;
-            curr = curr->next;
-        }cout<<len<<endl;
-        curr = head;
-        ListNode* first = NULL;ListNode* last = NULL;
-        vector<vector<ListNode*>>v;
-        int extra = len%k;
-        for(int i = 1;i<=len-extra;i+=k){
-            prev = NULL;
-            for(int j = i;j<k+i;j++){
-                if(j==i){
-                    first = curr;
+             if(i%k==0){
+                nextNode = curr->next;
+                curr->next = NULL;
+                ListNode* newhead = reverseList(temp);
+                cout<<newhead->val<<endl;
+                if(temp==head){
+                    head = newhead;
                 }
-                if(j==k+i-1){
-                    last = curr;
+                if(prevNode!=NULL){
+                    prevNode->next = newhead;
                 }
-                Next = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = Next;
-            }
-            v.push_back({first,last});
-            first = NULL,last = NULL; 
+                prevNode = temp;
+                curr = nextNode;
+                temp = nextNode;     
+                i = 1;  
+             }
+             else{
+                i+=1;
+                curr = curr->next;
+             }
         }
-        for(int i = 0;i<v.size();i++){
-            cout<<v[i][0]->val<<" "<<v[i][1]->val<<endl;
-        }
-         head = v[0][1];
-        if(v.size()>1){
-        for(int i = 0;i<v.size()-1;i++){
-           v[i][0]->next = v[i+1][1];
-        }
-        }
-        v[v.size()-1][0]->next = curr;
+        prevNode->next = temp;
         return head;
     }
 };
