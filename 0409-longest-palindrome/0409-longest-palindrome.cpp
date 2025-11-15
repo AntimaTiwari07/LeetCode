90545cc7b04cf65a16bed2cd659cac48dcbddf04
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-11-15 23:44:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (37.83%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -5,23 +5,18 @@ public:
         for(int i = 0;i<s.size();i++){
             mp[s[i]]++;
         }
-        int sum =0;
-        int odd = 0;
-        int temp = 0;
-        int total = 0;
-        int cnt = 0;
+        bool odd = false;
+        int ans = 0;
         for(auto i:mp){
             if(i.second%2==0){
-                sum+=(i.second);
+                ans+=i.second;
             }
-            else{   
-                 odd+=(i.second);
-                cnt+=1;
+            else{
+                ans+=(i.second-1);
+                odd = true;
             }
         }
-        cout<<odd<<endl;
-        if(odd==0 && temp==1)return sum+1;
-              if(cnt!=0)return sum+odd-cnt+1;
-              return sum+odd;
+        if(odd)return ans+1;
+        return ans;
     }
 };
