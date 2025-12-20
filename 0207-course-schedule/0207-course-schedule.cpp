11330817dae40f5ff2fc15fbfdbd9019be9a96ce
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-12-20 23:34:16 +0530

    Time: 0 ms (100%), Space: 19.4 MB (57.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+bool dfs(vector<vector<int>>&v,vector<int>&vis,int node,vector<int>&dfsstack){
+    vis[node]=1;
+    dfsstack[node]=1;
+    for(auto i:v[node]){
+        if(!vis[i]){
+            //cycle present
+            if(dfs(v,vis,i,dfsstack)==true)return true;
+        }
+        //visited but present in stack so cycle is present
+        else if(dfsstack[i]==1)return true;
+    }
+    dfsstack[node]=0;
+    return false;
+}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>v(numCourses);
         for(int i = 0;i<prerequisites.size();i++){
             v[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        //indegree find
-        vector<int>degree(numCourses,0);
-        for(int i = 0;i<v.size();i++){
-           for(int j = 0;j<v[i].size();j++){
-            degree[v[i][j]]++;
-           }
+        vector<int>vis(numCourses,0);
+         vector<int>dfsstack(numCourses);
+        for(int i = 0;i<numCourses;i++){
+           if(vis[i]==1)continue;
+            if(dfs(v,vis,i,dfsstack)==true)return false;
         }
-        //jiski indegree 0 he usko queue me daal do
-        queue<int>q;
-        for(int i = 0;i<degree.size();i++){
-            if(degree[i]==0)q.push(i);
-        }
-        int cnt = 0;
-        while(!q.empty()){
-            int x = q.front();
-            q.pop();
-            cnt+=1;
-            for(auto i:v[x]){
-                degree[i]--;
-                if(degree[i]==0)q.push(i);
-            }
-        }
-        if(cnt!=numCourses)return false;
         return true;
     }
 };
