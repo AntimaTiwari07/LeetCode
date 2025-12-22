f8ad149fcff907618ef26f5a8eaf92db13f08fa4
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-12-23 02:12:13 +0530

    Time: 52 ms (29.58%), Space: 114.9 MB (17.86%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>&v,vector<bool>&vis,int node){
-    vis[node] = true;
-    for(auto i:v[node]){
-        if(!vis[i])dfs(v,vis,i);
-    }
-}
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         vector<vector<int>>v(n);
         vector<int>reach(n,0);
@@ -13,7 +7,6 @@ void dfs(vector<vector<int>>&v,vector<bool>&vis,int node){
             v[edges[i][0]].push_back(edges[i][1]);
             reach[edges[i][1]]=1;
         }
-        vector<bool>vis(n,false);
         vector<int>ans;
         for(int i = 0;i<n;i++){
             if(reach[i]==0)ans.push_back(i);
