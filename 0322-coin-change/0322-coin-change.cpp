9c7ca031518708683912a3629300e0e37d2deca1
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-12-27 03:23:39 +0530

    Time: 62 ms (12.19%), Space: 72.7 MB (5.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-int solve(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
-  if(i==coins.size()|| amount<0)return 1e9;
-  if(amount==0)return 0;
-  if(dp[i][amount]!=-1)return dp[i][amount];
-  int not_take = solve(coins,amount,i+1,dp);
-  int take = 1e9;
-  if(amount>=coins[i]){
-     take =  1+ solve(coins,amount-coins[i],i,dp);
-  }
-  return dp[i][amount] = min(take,not_take);
+int s(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
+    if(i==coins.size())return 1e9 ;
+    if(amount==0)return 0;
+    if(dp[i][amount]!=-1)return dp[i][amount];
+    int take = 1e9;
+    int nottake = s(coins,amount,i+1,dp);
+    if(amount>=coins[i]){
+        take = 1+s(coins,amount-coins[i],i,dp);
+    }
+    return dp[i][amount] = min(take,nottake);
 }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>>dp(13,vector<int>(amount+1,-1));
-        int x =  solve(coins,amount,0,dp);
+        int x =  s(coins,amount,0,dp);
         if(x==1e9)return -1;
         return x;
     }
