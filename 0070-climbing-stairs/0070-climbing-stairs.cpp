59234f880105e7632fa563f5fd0c1567a7c84968
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-12-27 00:35:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (47.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
- long long combination(int n,int r){
-    if(r>n || r<0)return 0;
-    if(r==0 || r==n)return 1;
-    long long res = 1;
-    r = min(r,n-r);
-    for(int i = 0;i<r;i++){
-        res = res*(n-i)/(i+1);
-    }
-    return res;
- }
+int dp(int n,vector<int>&v){
+    if(n==0)return 1 ;
+    if(n<0)return 0;
+    if(v[n]!=-1)return v[n];
+     return v[n]=dp(n-1,v)+dp(n-2,v);
+      
+}
     int climbStairs(int n) {
-     int rem = n;
-     int sum = 0;
-     int count  = 0;
-     int size= 0;
-     while(rem>1){
-       rem = rem-2;
-       count+=1;
-       size = rem+count;
-      sum = sum + combination(size,rem) * combination(size-rem,count);
-     }
-     return sum+1;
+       vector<int>v(n+1,-1);
+        
+        return dp(n,v);
     }
 };
