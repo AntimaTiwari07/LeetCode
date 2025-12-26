de2a215ea3cbec289bb7cdf38faafc11cf48c595
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2025-12-27 00:41:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,9 +7,17 @@ int dp(int n,vector<int>&v){
      return v[n]=dp(n-1,v)+dp(n-2,v);
       
 }
+int tdp(int n){
+    vector<int>dp(n+1,0);
+    dp[0]=1;
+    dp[1] = 1;
+    for(int i = 2;i<=n;i++){
+      dp[i] = dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
     int climbStairs(int n) {
        vector<int>v(n+1,-1);
-        
-        return dp(n,v);
+        return tdp(n);
     }
 };
