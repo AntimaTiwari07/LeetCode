326774a3b5d472779af4753c13aeb2b197da9fb6
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-01-26 02:13:32 +0530

    Time: 125 ms (15.1%), Space: 149.4 MB (15.14%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -3,22 +3,24 @@ public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>>v;
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
-       vector<vector<int>>dp(nums1.size(),vector<int>(nums2.size(),0));
+     unordered_map<string,int>mp;
         q.push({nums1[0]+nums2[0],{0,0}});
-        dp[0][0] = 1;
+        string s = to_string(0)+" "+to_string(0);
         while(k>0){
             pair<int,pair<int,int>>p = q.top();
             q.pop();
             k--;
             pair<int,int>x = p.second;
             v.push_back({nums1[x.first],nums2[x.second]});
-            if(x.second+1<nums2.size() && dp[x.first][x.second+1] == 0){
+            s = to_string(x.first)+" "+to_string(x.second+1);
+            if(x.second+1<nums2.size() && mp[s]==0 ){
                 q.push({nums1[x.first]+nums2[x.second+1],{x.first,x.second+1}});
-                dp[x.first][x.second+1] = 1;
+                mp[s]++;
             }
-           if(x.first+1<nums1.size() && dp[x.first+1][x.second] ==0){ 
+            s = to_string(x.first+1)+" "+to_string(x.second);
+           if(x.first+1<nums1.size() && mp[s]==0 ){ 
             q.push({nums1[x.first+1]+nums2[x.second],{x.first+1,x.second}});
-            dp[x.first+1][x.second] = 1;
+            mp[s]++;
         }
         }
         return v;
