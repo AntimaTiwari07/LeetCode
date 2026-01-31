287026499aadcc79b86c9e121190fe2a1e79dc61
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-01-31 23:44:44 +0530

    Time: 11 ms (61.72%), Space: 23.5 MB (22.2%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int solve(vector<vector<int>>& matrix,int i,int j,vector<vector<int>>&dp){
+    if(i>=matrix.size() || i<0 || j>=matrix[0].size() || j<0)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    int left = 0,right=0,up=0,down=0;
+    if(j-1>=0 && matrix[i][j-1]>matrix[i][j]){
+        left =  1+ solve(matrix,i,j-1,dp);
+    }
+    if(j+1<matrix[0].size() && matrix[i][j+1]>matrix[i][j]){
+        right = 1+ solve(matrix,i,j+1,dp);
+    }
+    if(i-1>=0 && matrix[i-1][j]>matrix[i][j]){
+        up = 1+solve(matrix,i-1,j,dp);
+    }
+    if(i+1<matrix.size() && matrix[i+1][j]>matrix[i][j]){
+        down = 1+solve(matrix,i+1,j,dp);
+    }
+  return dp[i][j] = max({left,right,up,down});
+}
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int maxi = INT_MIN;
+        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),-1));
+        for(int i = 0;i<matrix.size();i++){
+            for(int j = 0;j<matrix[0].size();j++){
+                maxi = max(maxi,solve(matrix,i,j,dp)+1);
+            }
+        }
+        return maxi;
+    }
+};
