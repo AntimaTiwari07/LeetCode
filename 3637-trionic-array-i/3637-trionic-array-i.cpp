c66b66d253b896928901c6e21e346f7653d050d9
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-02-03 23:47:58 +0530

    Time: 3 ms (11.69%), Space: 27.6 MB (96.31%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+            int j = -1;
+            for(j = 0;j<nums.size()-1;j++){
+                if(nums[j]>nums[j+1])break;
+                else if(nums[j]==nums[j+1])return false;
+            } 
+            if(j==nums.size()-1 || j==0)return false;
+            int k;
+            for(k = j;k<nums.size()-1;k++){
+                if(nums[k]==nums[k+1])return false;
+                else if(nums[k]<nums[k+1])break;
+            }
+            cout<<k<<endl;
+            if(k==nums.size()-1 || j==k)return false;
+            int l =-1 ;
+            for(l = k;l<nums.size()-1;l++){
+                if(nums[l]>=nums[l+1])return false;
+            }
+            cout<<l<<endl;
+           return true;
+    }
+};
