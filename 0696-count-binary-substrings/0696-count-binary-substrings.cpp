76c1731040761996cfdd99f1d61cb98ff6310dd6
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-02-20 02:58:31 +0530

    Time: 3 ms (67.57%), Space: 13 MB (62.16%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -5,26 +5,28 @@ public:
         int i = 0;
         int j = 0;
         int cnt = 0;
+        int ans = 0;
+        int x = 0;
         while(j<s.size()){
             if(s[i]==s[j]){
                 cnt+=1;
                 j++;
             }
             else{
-                v.push_back(cnt);
+               if(x==0){
+                x = cnt;
+               }
+               else{
+                ans+= min(x,cnt);
+                x = cnt;
+               }
                 cnt = 0;
                 while(s[i]!=s[j]){
                     i++;
                 }
             }
         }
-        v.push_back(cnt);
-        int ans = 0;
-        for(int i = 0;i<v.size()-1;i++){
-            cout<<v[i]<<" ";
-         ans += min(v[i],v[i+1]);
-        }
-        cout<<v[v.size()-1]<<endl;;
-        return ans;
+        
+        return ans+min(x,cnt);
     }
 };
