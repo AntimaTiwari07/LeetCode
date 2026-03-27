ac963dd7d74555e40779c6979f732703439c9514
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-03-27 12:19:19 +0530

    Time: 0 ms (100%), Space: 31.2 MB (67.7%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,35 +1,12 @@
 class Solution {
 public:
-   void left(vector<vector<int>>& v){
-    for(int i = 0;i<v.size();i+=2){
-        int prev = v[i][v[0].size()-1];
-       for(int j = v[0].size()-2;j>=0;j--){
-        int curr = v[i][j];
-        v[i][j]=prev;
-        prev = curr;
-       }
-       v[i][v[0].size()-1] = prev;
-    }
-   }
-   void right(vector<vector<int>>& v){
-        for(int i = 1;i<v.size();i+=2){
-        int prev = v[i][0];
-       for(int j = 1;j<v[0].size();j++){
-        int curr = v[i][j];
-        v[i][j]=prev;
-        prev = curr;
-       }
-       v[i][0] = prev;
-    }
-   }
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>>v = mat;
-        int turn = k%(mat[0].size());
-        for(int i = 0;i<turn;i++){
-            left(v);
-            right(v);
+        int n = mat[0].size();
+        for(int i = 0;i<mat.size();i++){
+            for(int j = 0;j<mat[0].size();j++){
+                if(mat[i][j]!=mat[i][(j+k)%n])return false;
+            }
         }
-      if(v==mat)return true;
-      return false;
+        return true;
     }
 };
