966fa9295a1c821f31190dfa6c5c6a4363455fe0
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-01 09:17:31 +0530

    Time: 966 ms (26.88%), Space: 499.3 MB (63.53%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
 int f(vector<vector<int>>& grid, int k,int cost,int i,int j,vector<vector<vector<int>>>&dp){
-    if(i==grid.size()-1 && j==grid[0].size()-1){
-        if(grid[i][j]!=0)cost+=1;
-        if(cost>k)return -1e9;
-        return grid[i][j];
-    }
-    if(i<0 || j<0 || i>=grid.size()|| j>=grid[0].size() || cost>k) return -1e9;
+    if(i<0 || j<0 || i>=grid.size()|| j>=grid[0].size()) return -1e9;
     if(grid[i][j]!=0)cost+=1;
     if(cost>k)return -1e9;
+    if(i==grid.size()-1 && j==grid[0].size()-1){
+        return grid[i][j];
+    }
      if(dp[i][j][cost]!=-1)return dp[i][j][cost];
     return dp[i][j][cost] = max(grid[i][j]+f(grid,k,cost,i,j+1,dp) , grid[i][j]+f(grid,k,cost,i+1,j,dp));
 
