930bca52432f573abb2dd40973dc789bc829ce31
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-16 02:06:19 +0530

    Time: 3 ms (16.37%), Space: 8.6 MB (5.8%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -4,10 +4,11 @@ public:
        unordered_map<int,int>mp;
        if(n==1)return true;
         while(n!=1 && mp[n]<=1){
-        string s = to_string(n);
-        int num  = 0;
-        for(int i = 0;i<s.size();i++){
-            num = num + ((s[i]-'0')*(s[i]-'0'));
+        int temp = n;
+        int num = 0;
+        while(temp!=0){
+            num = num + pow((temp%10),2);
+            temp/=10;
         }
         if(num==1)return true;
        mp[num]++;
