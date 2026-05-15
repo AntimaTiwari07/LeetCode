25aa16610a302df71cea82a18a0464ea61b13c47
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-15 22:09:57 +0530

    Time: 0 ms (100%), Space: 13.9 MB (97.75%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int start = 0;
-        int end = nums.size()-1;
-        int  mid;
-        int ans = -1;
-        while(start<=end){
-            mid = start + (end-start)/2;
-            if(nums[end]>=nums[start]){
-                return nums[start];
+        int st = 0;
+        int end = (nums.size()/2)-1;
+        int mid;
+        int maxi = 0;
+        while(st<=end){
+            mid = st+(end-st)/2;
+            if(nums[mid]>=nums[maxi]){
+                maxi = mid;
+                st = mid+1;
             }
-            if(nums[mid]>=nums[end]){
-              start = mid+1;
-            }
-            else {
-                end = mid;
+            else{
+                end = mid-1;
             }
         }
-        return 0;
+        st = (nums.size()/2);
+        end = nums.size()-1;
+        int maxi2= st;
+        while(st<=end){
+            mid = st+(end-st)/2;
+            if(nums[mid]>=nums[maxi2]){
+                maxi2 = mid;
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        if(nums[maxi]<=nums[maxi2])return nums[(maxi2+1)%nums.size()];
+        return nums[(maxi+1)%nums.size()];
     }
 };
