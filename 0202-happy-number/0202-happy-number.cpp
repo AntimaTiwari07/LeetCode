9859d1ab568fdb8c473db1bd44b9fbbd9c486930
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-16 02:08:04 +0530

    Time: 0 ms (100%), Space: 8.4 MB (21.46%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool isHappy(int n) {
-       unordered_map<int,int>mp;
-       if(n==1)return true;
-        while(n!=1 && mp[n]<=1){
+       unordered_set<int>st;
+        while(n!=1){
         int temp = n;
         int num = 0;
         while(temp!=0){
             num = num + pow((temp%10),2);
             temp/=10;
         }
-        if(num==1)return true;
-       mp[num]++;
+        if(st.find(num)!=st.end())return false;
+        st.insert(num);
         n = num;
         }
-    return false;
+    return true;
     }
 };
