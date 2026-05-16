4ae266a8aaaf6fe8f77a68b0b0ba486f199db29f
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-17 00:14:41 +0530

    Time: 0 ms (100%), Space: 16.3 MB (27.34%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-void removedup(vector<int>& nums){
-    unordered_map<int,int>mp;
-    vector<int>v;
-    for(int i =0;i<nums.size();i++){
-        if(mp[nums[i]]==0){
-         v.push_back(nums[i]);
-        }
-        mp[nums[i]]++;
-    }
-    nums = v;
-}
-
     int findMin(vector<int>& nums) {
-        removedup(nums);
-  int st = 0;
-  int end = nums.size()/2-1;
-  int mid;
-  int maxi = st;
-  while(st<=end){
-    mid = st+(end-st)/2;
-    if(nums[mid]>=nums[maxi]){
-        maxi = mid;
-        st = mid+1;
-    }
-    else{
-        end = mid-1;
-    }
-  }
-  st = nums.size()/2;
-  end = nums.size()-1;
-  int maxi2 = st;
-  while(st<=end){
-    mid = st+(end-st)/2;
-     if(nums[mid]>=nums[maxi2]){
-        maxi2 = mid;
-        st = mid+1;
-    }
-    else{
-        end = mid-1;
-    }
-  }
-  if(nums[maxi2]>nums[maxi])return nums[(maxi2+1)%nums.size()];
-  return nums[(maxi+1)%nums.size()];
+        int st = 0;
+        int end = nums.size()-1;
+        int mid;
+        int idx = 0;
+        while(st<=end){
+            while( st<end && nums[st]==nums[st+1]) st+=1;
+            while(end>st && nums[end]==nums[end-1])end--;
+            mid = st+(end-st)/2;
+          if(nums[mid]<nums[idx]){
+            idx = mid;
+          }
+          if(nums[mid]>nums[end]){
+            st = mid+1;
+          }
+          else{
+            end = mid-1;
+          }
+        }
+       return  nums[idx];
     }
 };
