2c4f186d10ad07422ee9c3f420e8ed3b750277ab
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-17 02:19:02 +0530

    Time: 911 ms (11.84%), Space: 33.4 MB (14.82%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+  int bfs(unordered_map<string,vector<string>>&v,string beginWord,string endWord,unordered_map<string,int>&mp){
+     queue<string>q;
+     q.push(beginWord);
+     q.push("0");
+     mp[beginWord] = 1;
+     int cnt = 0;
+     bool exist = false;
+     while(!q.empty()){
+        string st = q.front();
+        q.pop();
+        if(st==endWord){
+            exist = true;
+            break;
+        }
+        if(st=="0"){
+            cnt+=1;
+        if(!q.empty())q.push("0");
+        continue;
+        }
+       for(int i = 0;i<v[st].size();i++){
+        if(mp[v[st][i]]==0){
+            q.push(v[st][i]);
+            mp[v[st][i]] = 1;
+        }
+       }
+   
+     }
+    if(!exist)return 0;
+     return cnt+1;
+  }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,vector<string>>v;
+        bool found = false;
+        bool add = true;
+        for(int i = 0;i<wordList.size();i++){
+            if(wordList[i]==beginWord){add = false;}
+            if(endWord==wordList[i]){found = true;break;}
+            
+        }
+        if(!found)return 0;
+        if(add)wordList.push_back(beginWord);
+        for(int i = 0;i<wordList.size();i++){
+            string next = wordList[i];
+            for(int j = 0;j<wordList.size();j++){
+                int diff = 0;
+                for(int k = 0;k<wordList[j].size();k++){
+                    if(wordList[j][k]!=next[k])diff++;
+                }
+                if(diff==1)v[next].push_back(wordList[j]);
+            }
+        }
+        //bfs
+        unordered_map<string,int>mp;
+        return bfs(v,beginWord,endWord,mp);
+    }
+};
