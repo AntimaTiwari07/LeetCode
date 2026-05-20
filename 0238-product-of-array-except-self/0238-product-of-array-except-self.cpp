be5a2c9a9a5cc925493810737867551cceef29db
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-20 17:25:13 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58.63%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-       vector<int>ans(nums.size(),0);
-       int product = nums[0];
-       int product_without_zero = 1;
-       for(int i = 1;i<nums.size();i++){
-        product = product*nums[i];
-       }
-       for(int i = 0;i<nums.size();i++){
-        if(nums[i]==0) continue;
-        else
-        product_without_zero = product_without_zero*nums[i];
-       }
-       int count = 0;
-       for(int i = 0;i<nums.size();i++){
-            if(nums[i]==0) count++;
-       }
-       int temp;
-       for(int i = 0;i<nums.size();i++){
-        if(count>1){
-            return ans;
-        }
-        if(nums[i]==0){
-            ans[i]=(product_without_zero ); 
-        }else 
-        ans[i] = (product/nums[i]);
-       }
-       return ans;
+      int prod= 1;
+      int zero = 0;
+      bool val= false;
+      for(int i = 0;i<nums.size();i++){
+       if(nums[i]!=0){ prod *=  nums[i];val = true;}
+       else {zero +=1;}
+      }  
+      vector<int>ans(nums.size());
+     for(int i = 0;i<nums.size();i++){
+        if(nums[i]!=0 && zero>=1){ans[i] = 0;}
+       else if(!val)ans[i] =0;
+       else if(zero>1 && nums[i]==0) ans[i] = 0;
+       else if(zero==1 && nums[i]==0)ans[i] = prod;
+        else ans[i] = prod/nums[i];
+     }
+     return ans;
     }
 };
