e8e51cb6c5ce36f9bbc7730ce65ba1e026729e49
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-22 23:57:32 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-   int st  = 0;
-   int end = nums.size()-1;
-   int mini = nums[st];
-   int mid;
-   int idx = 0;
-   while(st<=end){
-    mid = st+(end-st)/2;
-    if(nums[mid]<mini){
-        idx = mid;
-        end = mid-1;
-    }
-    else{
-        st = mid+1;
-    }
-   }
-   cout<<idx<<endl; 
-   if(idx-1>=0 && target>=nums[0] && target<=nums[idx-1]) {
-    st = 0;
-    end = idx-1;
-   }
-   else if(target>=nums[idx] && target<=nums[nums.size()-1]){
-    st  =idx;
-    end = nums.size()-1;
-   }
-   else return -1;
-   while(st<=end){
-    mid = st+(end-st)/2;
-    if(nums[mid]==target)return mid;
-    else if(nums[mid]<target){
-        st = mid+1;
-    }
-    else{
-        end = mid-1;
-    }
-   }
-return -1;
+        int st = 0;
+        int end = nums.size()-1;
+        int mid;
+        while(st<=end){
+            mid = st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[st]<=nums[mid]){
+                //left part sorted 
+                if(target>=nums[st] && target<nums[mid]){
+                    end = mid-1;
+                }else{
+                    st = mid+1;
+                }
+            }
+            else{
+                //right part sorted
+                if(target<=nums[end] && target>nums[mid]){
+                    st= mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
     }
 };
