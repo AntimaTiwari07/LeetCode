344d9fad5ee366eba74f9c9966e3aa365b40768c
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-05-27 00:20:18 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         unordered_map<char,int>mp;
-        unordered_map<char,int>mp2;
         int cnt = 0;
         for(int i = 0;i<word.size();i++){
-            if(word[i]<='z' && word[i]>='a')mp[word[i]]++;
+            mp[word[i]]++;
         }
         
         for(int j = 0;j<word.size();j++){
-            if(word[j]<='Z' && word[j]>='A' && mp[(word[j]-'A')+'a']!=0){
-                mp2[word[j]]++;
+            if(mp[word[j]]>0 && mp[(word[j]-'a')+'A']>0){
+               cnt+=1;
+               mp[word[j]] = 0;
             }
         }
-        return mp2.size();
+        return cnt;
     }
 };
