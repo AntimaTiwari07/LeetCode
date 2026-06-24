1c6a53279dcf312ef8a94539ae4cbdd9a2c7a66e
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-06-24 17:46:27 +0530

    Time: 0 ms (100%), Space: 29.9 MB (29.64%) - LeetHub

--- 0593-valid-square/0593-valid-square.cpp
+++ 0593-valid-square/0593-valid-square.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    double dist(vector<int>& p1, vector<int>& p2) {
-        return (p2[1] - p1[1]) * (p2[1] - p1[1]) +
-               (p2[0] - p1[0]) * (p2[0] - p1[0]);
-    }
-
-    bool check(vector<int>& p1, vector<int>& p2, vector<int>& p3,
-               vector<int>& p4) {
-        return dist(p1, p2) > 0 && dist(p1, p2) == dist(p2, p3) &&
-               dist(p2, p3) == dist(p3, p4) && dist(p3, p4) == dist(p1, p4) &&
-               dist(p1, p3) == dist(p2, p4);
-    }
-
-    void swap(vector<vector<int>>& p, int x, int y) {
-        vector<int> temp = p[x];
-        p[x] = p[y];
-        p[y] = temp;
-    }
-
-    bool checkAllPermutations(vector<vector<int>>& p, int l) {
-        if (l == 4) {
-            if (p[0] == p[1] || p[1] == p[2] || p[2] == p[3] || p[3] == p[0] ||
-                p[0] == p[2] || p[1] == p[3])
-                return false;
-            return check(p[0], p[1], p[2], p[3]);
-        } else {
-            bool res = false;
-            for (int i = l; i < 4; i++) {
-                swap(p, l, i);
-                res |= checkAllPermutations(p, l + 1);
-                swap(p, l, i);
-                if (res) return true;
-            }
-            return res;
+int dis(vector<int>&a,vector<int>&b){
+    int dx = b[0]-a[0];
+    int dy = b[1]-a[1];
+    return (dx*dx + dy*dy);
+}
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        unordered_map<int,int>mp;
+       mp[dis(p1,p2)]++;
+       mp[dis(p1,p3)]++;
+       mp[dis(p1,p4)]++;
+       mp[dis(p2,p3)]++;
+       mp[dis(p2,p4)]++;
+       mp[dis(p3,p4)]++;
+    
+      if(mp.size()==2){
+        vector<int>cnt(2);
+        int k = 0;
+        for(auto i:mp){
+            cnt[k++] = i.first;
         }
-    }
-
-    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3,
-                     vector<int>& p4) {
-        vector<vector<int>> p = {p1, p2, p3, p4};
-        return checkAllPermutations(p, 0);
+        sort(cnt.begin(),cnt.end());
+        if(cnt[1]!=2*cnt[0])return false;
+        return true;
+      }
+      return false;
     }
 };
