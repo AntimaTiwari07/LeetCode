df2494636211eee5b2ffc650b951fe26c6c570e3
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-07-04 00:31:27 +0530

    Time: 5 ms (94.87%), Space: 33.5 MB (39.97%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,45 +1,35 @@
 class Solution {
-    static constexpr int DIRS[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dis(m, vector<int>(n, INT_MAX));
-
-        deque<pair<int, int>> q;
-        q.emplace_front(0, 0);
-        dis[0][0] = grid[0][0];
-
-        while (!q.empty()) {
-            auto [cx, cy] = q.front();
-            q.pop_front();
-            // the first time it leaves the queue, the shortest distance is
-            // guaranteed
-            if (cx == m - 1 && cy == n - 1) {
-                return true;
-            }
-
-            for (auto& [dx, dy] : DIRS) {
-                int nx = cx + dx, ny = cy + dy;
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
-                    continue;
-                }
-                int cost = dis[cx][cy] + grid[nx][ny];
-                // pruning: the new distance does not meet health requirements
-                if (cost >= health) {
-                    continue;
-                }
-                if (cost < dis[nx][ny]) {
-                    dis[nx][ny] = cost;
-                    if (grid[nx][ny] == 0) {
-                        q.emplace_front(nx, ny);
-                    } else {
-                        q.emplace_back(nx, ny);
-                    }
-                }
-            }
+        //0-1 bfs
+        int size = grid[0].size()*(grid.size()-1)+(grid[0].size()-1);
+      vector<vector<int>>src(grid.size(),vector<int>(grid[0].size(),INT_MAX));
+      deque<pair<int,pair<int,int>>>dq;
+      dq.push_front({grid[0][0],{0,0}});
+      while(!dq.empty()){
+        pair<int,pair<int,int>>p= dq.front();
+        dq.pop_front();
+        int cost = p.first;
+        pair<int,int>x = p.second;
+        int i = x.first,j = x.second;
+        if(j+1<grid[0].size() && cost+grid[i][j+1]<src[i][j+1]){
+            src[i][j+1] = cost+grid[i][j+1];
+            (grid[i][j+1]==1)?dq.push_back({src[i][j+1],{i,j+1}}):dq.push_front({src[i][j+1],{i,j+1}});
         }
-
-        return false;
+        if(i+1<grid.size() && cost+grid[i+1][j]<src[i+1][j]){
+            src[i+1][j] = cost+grid[i+1][j];
+            (grid[i+1][j]==1)?dq.push_back({src[i+1][j],{i+1,j}}):dq.push_front({src[i+1][j],{i+1,j}});
+        }
+        if(j-1>=0 && cost+grid[i][j-1]<src[i][j-1]){
+            src[i][j-1] = cost+grid[i][j-1];
+            (grid[i][j-1]==1)?dq.push_back({src[i][j-1],{i,j-1}}):dq.push_front({src[i][j-1],{i,j-1}});
+        }
+               if(i-1>=0 && cost+grid[i-1][j]<src[i-1][j]){
+            src[i-1][j] = cost+grid[i-1][j];
+            (grid[i-1][j]==1)?dq.push_back({src[i-1][j],{i-1,j}}):dq.push_front({src[i-1][j],{i-1,j}});
+        }
+      }
+      if(health-src[grid.size()-1][grid[0].size()-1] >=1)return true;
+      return false;
     }
 };
