fcf11e26d2d72719c1c3e34b0160df8e73c370ba
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-07-10 13:45:47 +0530

    Time: 226 ms (7.46%), Space: 259 MB (8.29%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,13 +1,12 @@
 class DSU{
-    vector<int> parent;
-
-public:
+     vector<int>parent;
+    public:
+   
     DSU(int n){
-        for(int i = 0; i < n; i++){
+        for(int i = 0;i<n;i++){
             parent.push_back(i);
         }
     }
-
     int find(int x){
         if(parent[x] == x){
             return x;
@@ -15,45 +14,35 @@ public:
 
         return parent[x] = find(parent[x]);
     }
-
-    void merge(int x, int y){
-        int parX = find(x);
-        int parY = find(y);
-
-        parent[parX] = parY;
+    void merge(int x,int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if(x_parent!=y_parent){
+            parent[x_parent]=y_parent;
+        }
     }
 };
-
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        priority_queue<pair<int, int>> pq;
-
-        for(int i = 0; i < n; i++){
-            pq.push({nums[i], i});
+        priority_queue<pair<int,int>>pq;
+        for(int i = 0;i<nums.size();i++){
+            pq.push({nums[i],i});
         }
-
         DSU dsu(n);
-
-        pair<int, int> temp = pq.top();
-
+        pair<int,int>temp = pq.top();
         while(!pq.empty()){
-            pair<int, int> curr = pq.top();
+            pair<int,int>curr = pq.top();
             pq.pop();
-
-            if(abs(temp.first - curr.first) <= maxDiff){
-                dsu.merge(temp.second, curr.second);
+            if(abs(temp.first-curr.first)<=maxDiff){
+                dsu.merge(temp.second,curr.second);
             }
-
             temp = curr;
         }
-
-        vector<bool> sol;
-
-        for(auto &it : queries){
-            sol.push_back(dsu.find(it[0]) == dsu.find(it[1]));
+        vector<bool>ans(queries.size(),false);
+        for(int i = 0;i<queries.size();i++){
+            if(dsu.find(queries[i][0])==dsu.find(queries[i][1]))ans[i] = true;      
         }
-
-        return sol;
+        return ans;
     }
 };
