5aecef0f43558d436e5d7aa7fa41ef6bb2ef26f3
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-08-01 18:15:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-int solve(int i,int j ,vector<int>& nums,vector<vector<int>>&dp){
-    if(i>j){
-       return 0;
-    }
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    int player1 =nums[i] - solve(i+1,j,nums,dp);
-    int player2 =  nums[j] - solve(i,j-1,nums,dp);
-     return dp[i][j] = max(player1,player2);
+int solve(vector<int>& nums,int i,int j,vector<vector<int>>&dp){
+    if(i>j)return 0;
+    if(i==j)return nums[i];
+    if(dp[i][j]!=-1)return dp[i][j];
+    int take_i = nums[i]+min(solve(nums,i+2,j,dp),solve(nums,i+1,j-1,dp));
+    int take_j = nums[j]+min(solve(nums,i+1,j-1,dp),solve(nums,i,j-2,dp));
+    return dp[i][j] = max(take_i,take_j);
 }
     bool predictTheWinner(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0;i<nums.size();i++){
+            sum+=nums[i];
+        }
         vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return solve(0,nums.size()-1,nums,dp)>=0 ?true : false;
+        int p1 = solve(nums,0,nums.size()-1,dp);
+        int p2 = sum-p1;
+        if(p1>=p2)return true;
+        return false;
     }
 };
