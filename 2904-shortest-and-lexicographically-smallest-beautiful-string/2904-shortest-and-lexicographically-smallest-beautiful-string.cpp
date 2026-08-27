972f7d3752489d49573786b1ae6b98953f35d9d0
Antima_Tiwari_ <153281706+AntimaTiwari07@users.noreply.github.com>
2026-08-27 10:18:35 +0530

    Time: 19 ms (6.91%), Space: 18 MB (5.88%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        string ans = string(101,1);
-        int size = INT_MAX;
-        for(int i = 0;i<s.size();i++){
-            int cnt = 0;
-            string str = "";
-            for(int j = i;j<s.size();j++){
+        string st= "";
+        string ans = string(101,'1');
+        int cntt = 0;
+        for(int i = 0;i<k-1;i++){
+            if(s[i]=='1')cntt+=1;
+            st+=s[i];
+        }
+        int size = k;
+        for(int i = k-1;i<s.size();i++){
+            st+=s[i];
+            if (s[i]=='1')cntt++;
+            // cout<<"st = "<<st<<endl;
+            string str=st;
+            int cnt = cntt;
+            cout<<"st = "<<st<<endl;
+            for(int j = i+1;j<s.size();j++){
+                if(cnt==k){
+                        ans = min(ans,str);
+                        //  cout<<" and = "<<ans<<endl;
+                }  
                 if(s[j]=='1')cnt+=1;
                 str+=s[j];
-                if(cnt==k){
-                    if(str.size()<size){
-                        size = str.size();
-                        ans = str;
-                    }
-                    else if(str.size()==size){
-                        size = str.size();
-                        ans = min(ans,str);
-                    }
+                if(str[0]=='1')cnt-=1;
+                str = string(str.begin()+1,str.end());
+                //   cout<<str<<" "<<cnt<<"    ";
                 }
+                if(cnt==k){
+                        ans = min(ans,str);
+                        //  cout<<" and = "<<ans<<endl;
+                }  
+                 if(ans.size()!=101)return ans;
             }
-        }
         if(ans.size()==101)return "";
         return ans;
     }
